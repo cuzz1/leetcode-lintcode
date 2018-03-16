51a85bce5d979346b057f67bb386cbcb40e30000
cuzz1 <libing775544@163.com>
2018-03-16 13:39:33 +0800

    Initial commit

---
+++ Solution001.java
@@ -0,0 +1,12 @@
+
+public class Solution001 {
+    /**
+     * @param a: An integer
+     * @param b: An integer
+     * @return: The sum of a and b
+     */
+    public int aplusb(int a, int b) {
+        // write your code here
+    	return 0;
+    }
+}
---
+++ Solution002.java
@@ -0,0 +1,29 @@
+
+public class Solution002 {
+    /*
+     * @param n: An integer
+     * @return: An integer, denote the number of trailing zeros in n!
+     */
+	public static void main(String[] args) {
+		System.out.println(trailingZeros(100));
+	}
+    public static long trailingZeros(long n) {
+        // write your code here, try to do it without arithmetic operators.
+    	
+    	// 分解成2*5来算 统计5的个数就可以
+    	// 25 中有2个5
+    	long count = 0;
+    	
+    	// 比如 n = 100 第一次可以分为5 x 20 
+    	// 在加上 20 = 5 x 4 中的4就可以
+    	long t = 5;
+    	while(t <= n){
+    		count += n / t;
+    		n /= 5;
+    	}
+    	
+
+    	
+    	return count;
+    }
+}
