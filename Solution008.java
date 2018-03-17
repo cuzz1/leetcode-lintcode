663864608e3b2721f53a4af2bc8a51b054335e62
cuzz1 <libing775544@163.com>
2018-03-17 23:21:43 +0800

    008

---
+++ Solution008.java
@@ -0,0 +1,30 @@
+
+public class Solution008 {
+    /**
+     * @param str: An array of char
+     * @param offset: An integer
+     * @return: nothing
+     */
+	public static void main(String[] args) {
+		char[] str = {'a','b','c'};
+		rotateString(str, 4);
+		System.out.println(str);
+		
+	}
+    public static void rotateString(char[] str, int offset) {
+        // write your code here
+    	int N = str.length;
+    	char[] str2 = new char[str.length];
+    	for(int i = 0; i < N; i++){
+    		str2[i] = str[i];
+    	}
+//    	int off = offset % N;
+    	
+    	for(int i = 0; i < N; i++){
+    		if(offset != 0){
+    			str[(i+offset) % N] = str2[i];
+    		}
+    			
+    	}   	
+    }
+}
