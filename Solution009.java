9bf49af31a7cd1e6b727d9ddd3b1809219af2f78
cuzz1 <libing775544@163.com>
2018-03-18 17:06:27 +0800

    009

---
+++ Solution009.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution009 {
+    /**
+     * @param n: An integer
+     * @return: A list of strings.
+     */
+    public List<String> fizzBuzz(int n) {
+        // write your code here
+    	List<String> list = new ArrayList();
+    	for(int i = 1; i <=n; i++){
+    		if(i % 3 == 0 && i % 5 != 0){
+    			list.add("fizz");
+    		}else if( i % 5 == 0 && i % 3 != 0){
+    			list.add("buzz");
+    		}else if( i % 5 == 0 && i % 3 == 0){
+    			list.add("fizz buzz");
+    		}else{
+    			list.add(i + "");
+    		}
+    	}
+    	return list;
+    }
+}
