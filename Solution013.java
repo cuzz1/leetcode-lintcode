01b0acfb90b47db5742de2a19dcc278298533d8e
cuzz1 <libing775544@163.com>
2018-03-19 16:01:14 +0800

    013

--- Solution013.java
+++ Solution013.java
@@ -1,4 +1,27 @@
 
 public class Solution013 {
-
+    /*
+     * @param source: source string to be scanned.
+     * @param target: target string containing the sequence of characters to match
+     * @return: a index to the first occurrence of target in source, or -1  if target is not part of source.
+     */
+    public int strStr(String source, String target) {
+        // write your code here
+    	if(source == null || target == null){
+    		return -1;
+    	}
+    	
+    	for(int i = 0; i < source.length() - target.length() + 1; i++){
+    		int j;
+    		for(j = 0; j < target.length(); j++){
+    			if(source.charAt(i + j) != target.charAt(j)){
+    				break;
+    			}
+    		}
+    		if (j == target.length()){
+    			return i;
+    		}
+    	}
+    	return -1;
+    }
 }
