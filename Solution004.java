5f87ca880f0add35304ce1195b88039749571a6e
cuzz1 <libing775544@163.com>
2018-03-21 10:49:16 +0800

    004

--- Solution004.java
+++ Solution004.java
@@ -5,32 +5,40 @@ public class Solution004 {
      * @return: the nth prime number as description.
      */
 	public static void main(String[] args) {
-		int count = nthUglyNumber(10);
+		int count = nthUglyNumber(41);
 		System.out.println(count);
 	}
     public static int nthUglyNumber(int n) {
         // write your code here
-    	if(n < 0){
-    		return 0;
-    	}
-    	int count = 0;
-    	for(int i = 1; i <= n; i++){
-    		int flag = i;
-    		while(flag % 2 == 0){
-    			flag /= 2;
-    		}
-    		while(flag % 3 == 0){
-    			flag /= 3;
-    		}
-    		while(flag % 5 == 0){
-    			flag /= 5;
-    		}
-    		if(flag == 1){
-    			count++;
-    		}
-    	}
+    	int[] num = new int[n];
+    	// 添加第一个丑数
+    	num[0] = 1;
+    	int p2 = 0, p3 = 0, p5 = 0;
     	
-    	return count;
-    	
+    	for(int i = 1; i < n; i++){
+    		// 比较的数
+    		int lastNumber = num[i-1];
+    		
+    		while(num[p2]*2 <= lastNumber){
+    			p2++;
+    		}
+    		while(num[p3]*3 <= lastNumber){
+    			p3++;
+    		}
+    		while(num[p5]*5 <= lastNumber){
+    			p5++;
+    		}
+    		
+    		int minNumber = min(min(num[p2]*2, num[p3]*3), num[p5]*5);
+    		num[i] = minNumber;
+    	}
+    	return num[n-1];
     }
+	private static int min(int i, int j) {
+		if(i >= j){
+			return j;
+		}else{
+			return i;
+		}
+	}
 }
