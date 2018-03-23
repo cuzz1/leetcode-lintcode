b898bc1e39465ef0f476bbd75f641cdf8eeda843
cuzz1 <libing775544@163.com>
2018-03-23 23:02:30 +0800

    022

---
+++ Solution022.java
@@ -0,0 +1,17 @@
+
+public class Solution022 {
+    // @param nestedList a list of NestedInteger
+    // @return a list of integer
+    public List<Integer> flatten(List<NestedInteger> nestedList) {
+        // Write your code here
+        List<Integer> list = new ArrayList<Integer>();
+        for(NestedInteger ele: nestedList){
+            if(ele.isInteger()){
+                list.add(ele.getInteger());
+            }else{
+                list.addAll(flatten(ele.getList()));
+            }
+        }
+        return list;
+    }
+}
