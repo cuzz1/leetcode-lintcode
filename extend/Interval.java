299c6610f640c75a88af0286f5e1b0471ab3782f
cuzz1 <cuzz1234@163.com>
2018-03-26 17:20:37 +0800

    030

--- extend/Interval.java
+++ extend/Interval.java
@@ -1,5 +1,10 @@
 package extend;
 
 public class Interval {
-
+    public int start;
+	public int end;
+	Interval(int start, int end) {
+    	this.start = start;
+    	this.end = end;
+    }
 }
--- src/Solution030.java
+++ src/Solution030.java
@@ -1,5 +1,31 @@
 package src;
 
-public class Solution030 {
+import java.util.ArrayList;
+import java.util.List;
 
+import extend.Interval;
+
+public class Solution030 {
+    /*
+     * @param intervals: Sorted interval list.
+     * @param newInterval: new interval.
+     * @return: A new interval list.
+     */
+    public List<Interval> insert(List<Interval> intervals, Interval newInterval) {
+    	List<Interval> result = new ArrayList<Interval>();
+    	int insertPos = 0;
+    	for(Interval interval: intervals){
+    		if(interval.end < newInterval.start){
+    			result.add(interval);
+    			insertPos++;
+    		}else if(interval.start > newInterval.end){
+    			result.add(interval);
+    		}else{
+    			newInterval.start = Math.min(interval.start, newInterval.start);
+    			newInterval.end = Math.max(interval.end, newInterval.end);
+    		}
+    	}
+    	result.add(insertPos, newInterval);
+    	return result;
+    }
 }
