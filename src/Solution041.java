84d148b17ca79fb38e7f73e3a1ca73067faa59c1
cuzz1 <cuzz1234@163.com>
2018-03-27 22:20:39 +0800

    041

---
+++ src/Solution041.java
@@ -0,0 +1,26 @@
+package src;
+
+public class Solution041 {
+    /**
+     * @param nums: A list of integers
+     * @return: A integer indicate the sum of max subarray
+     */
+	public static void main(String[] args) {
+		int[] nums = {1, 3, -1, -2, 6,-4, 4};
+		int result = maxSubArray(nums);
+		System.out.println(result);
+	}
+    public static int maxSubArray(int[] nums) {
+        // write your code here
+        int temp = 0;
+ 
+        for(int i = 0; i < nums.length; i++){
+        	int addedNums = 0;
+        	for(int j = i; j < nums.length; j++){
+        		addedNums += nums[j];
+        		temp = Math.max(temp, addedNums);
+        	}
+        }
+        return temp;
+    }
+}
