c4c8e961b08ad213c79631f679c63baabc9d7ed4
cuzz1 <cuzz1234@163.com>
2018-03-29 21:20:59 +0800

    145

---
+++ src/Solution145.java
@@ -0,0 +1,12 @@
+package src;
+
+public class Solution145 {
+    /**
+     * @param character: a character
+     * @return: a character
+     */
+    public char lowercaseToUppercase(char character) {
+        // write your code here
+    	return (char) (character - 'a' + 'A');
+    }
+}
