c2866fa20e349cbf5c7a416a84714d9603507b12
cuzz1 <cuzz1234@163.com>
2018-03-29 18:06:11 +0800

    053

---
+++ src/Solution053.java
@@ -0,0 +1,36 @@
+package src;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution053 {
+    /*
+     * @param s: A string
+     * @return: A string
+     */
+	public static void main(String[] args) {
+		String s = " the world is so big that i want to see it  ";
+		String str = reverseWords(s);
+		System.out.println(str);
+	}
+    public static String reverseWords(String s) {
+        // write your code here
+    	if (s.length() == 0 || s.equals(" ")){
+    		return "";
+    	}
+    	
+    	String[] array = s.split(" ");
+    	StringBuilder sb = new StringBuilder();
+    	for (int i = array.length - 1; i >= 0; i--){
+    		if (!array[i].equals(" ")){  			
+    			if(sb.length() > 0){
+    				sb.append(" ");
+    			}
+    			sb.append(array[i]);
+    		}
+    	}
+    	return sb.toString();
+    }
+
+
+}
