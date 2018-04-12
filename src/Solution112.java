f2f0bc3123743aa2b1269b56f29b0b412098fe00
cuzz1 <cuzz1234@163.com>
2018-04-12 12:55:13 +0800

    112

---
+++ src/Solution112.java
@@ -0,0 +1,29 @@
+package src;
+
+import extend.ListNode;
+
+public class Solution112 {
+    /**
+     * @param head: head is the head of the linked list
+     * @return: head of linked list
+     */
+    public ListNode deleteDuplicates(ListNode head) {
+    	
+    	if (head == null) {
+    		return null;
+    	}
+    	
+    	// 定义一个指针
+    	ListNode p= head;
+    	
+    	// 循环链表
+    	while (p.next != null) {
+    		if (p.val == p.next.val) {
+    			p.next = p.next.next;
+    		} else {
+    			p = head.next;
+    		}
+    	}
+    	return head;
+    }
+}
