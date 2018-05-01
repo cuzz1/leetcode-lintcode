a532f41a23d85dbaccfef30defc553d620d36bc2
cuzz1 <cuzz1234@163.com>
2018-05-01 09:50:33 +0800

    统计数字

--- src/Solution003.java
+++ src/Solution003.java
@@ -13,7 +13,22 @@ public class Solution003 {
     public static int digitCounts(int k, int n) {
         // write your code here
     	int count = 0;
-    	
+    	for (int i = 0; i <= n; i++) {
+    		count += singleCount(k, i);
+    	}
     	return count;
     }
-};
+	private static int singleCount(int k, int i) {
+		if (i == 0 && k == 0) {
+			return 1;
+		}
+		int count = 0;
+		while (i > 0) {
+			if (i % 10 == k) {
+				count++;
+			}
+			i = i / 10;
+		}
+		return count;
+	}
+}
