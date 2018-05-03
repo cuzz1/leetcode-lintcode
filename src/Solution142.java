d9bc44ad87d38df110913dbb5b7fadf8e3a1ccbf
cuzz1 <cuzz1234@163.com>
2018-05-03 16:50:54 +0800

    142

---
+++ src/Solution142.java
@@ -0,0 +1,14 @@
+package src;
+
+public class Solution142 {
+    /**
+     * @param n: An integer
+     * @return: True or false
+     */
+    public boolean checkPowerOf2(int n) {
+    	if (n < 0) {
+    		return false;
+    	}
+    	return (n & (n-1)) == 0;
+    }
+}
