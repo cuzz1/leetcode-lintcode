7ddaf72772df61b06a6609211da96ee3dbd92022
cuzz1 <cuzz1234@163.com>
2018-05-14 00:39:35 +0800

    173

---
+++ src/Slution173.java
@@ -0,0 +1,31 @@
+package src;
+
+import extend.ListNode;
+
+public class Slution173 {
+	/**
+	 * @param head: The first node of linked list.
+	 * @return: The head of linked list.
+	 */
+	public ListNode insertionSortList(ListNode head) {
+		// write your code here
+		ListNode node = new ListNode(0);
+		
+		
+		while(head != null) {
+			ListNode cur = node;
+
+			while(cur.next != null && head.val >= cur.next.val) {
+					cur = cur.next;
+			} 
+//			ListNode temp = head.next;
+//			head.next = cur.next;
+//			cur.next = head;
+//			head = temp;
+			
+
+			
+		}
+		return node.next;
+	}
+}
