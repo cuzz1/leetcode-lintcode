4908bfacb7d7c101778aa884955b5776be4747e1
cuzz1 <cuzz1234@163.com>
2018-05-14 19:49:00 +0800

    204

--- src/Solution204.java
+++ src/Solution204.java
@@ -1,6 +1,20 @@
 package src;
 
 public class Solution204 {
+    /**
+     * @return: The same instance of this class every time
+     */
 	
+	private static Solution204 solution;
+	private Solution204() {
+		
+	}
 
+    public static Solution204 getInstance() {
+        // write your code here
+    	if (solution == null) {
+    		solution = new Solution204();
+    	}
+    	return solution;
+    }
 }
