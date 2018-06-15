fe57f5dd92152f961b57deb8a92881fb6eef1931
cuzz1 <cuzz1234@163.com>
2018-06-15 12:48:24 +0800

    1042

--- src/SolutionI042.java
+++ src/SolutionI042.java
@@ -1,6 +1,10 @@
 package src;
 
 public class SolutionI042 {
+    /**
+     * @param matrix: the given matrix
+     * @return: True if and only if the matrix is Toeplitz
+     */
     public boolean isToeplitzMatrix(int[][] matrix) {
         if (matrix == null) {
             return false;
@@ -10,17 +14,16 @@ public class SolutionI042 {
             return true;
         }
         
-        int rows = matrix.length - 1;
-        int columns = matrix[0].length - 1;
+        int M = matrix.length - 1;
+        int N = matrix[0].length - 1;
         
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < columns; j++) {
+        for (int i = 0; i < M; i++) {
+            for (int j = 0; j < N; j++) {
                 if (matrix[i][j] != matrix[i+1][j+1]) {
                     return false;
                 }
             }
         }
         return true;
-        
     }
 }
