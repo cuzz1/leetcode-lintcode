6a8b4954340e0a1dd2e510acfd3952511c9cabe5
cuzz1 <cuzz1234@163.com>
2018-06-16 22:24:13 +0800

    423

---
+++ src/Solution423.java
@@ -0,0 +1,38 @@
+package src;
+
+public class Solution423 {
+    /**
+     * @param s: A string
+     * @return: whether the string is a valid parentheses
+     */
+    public boolean isValidParentheses(String s) {
+        Stack<Character> stack = new Stack<>();
+        
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == '(' || c == '[' || c == '{') {
+                stack.push(c);
+            } else {
+                if (stack.isEmpty()){
+                    return false;
+                }
+                
+                char topChar = stack.pop();
+                
+                if (c == ')' && topChar != '(') {
+                    return false;
+                }
+                if (c == ']' && topChar != '[') {
+                    return false;
+                }
+                if (c == '}' && topChar != '{') {
+                    return false;
+                }
+                
+            }
+            
+        }
+        return stack.isEmpty();
+    }
+    
+}
