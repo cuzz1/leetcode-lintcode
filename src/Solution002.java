71e671facbdcc6d53c41981bd21b7b19c540b13f
cuzz1 <cuzz1234@163.com>
2018-06-16 15:59:11 +0800

    002

--- src/Solution002.java
+++ src/Solution002.java
@@ -5,22 +5,13 @@ public class Solution002 {
      * @param n: An integer
      * @return: An integer, denote the number of trailing zeros in n!
      */
-	public static void main(String[] args) {
-		System.out.println(trailingZeros(100));
-	}
-    public static long trailingZeros(long n) {
-    	
-    	// 分解成2*5来算 统计5的个数就可以
-    	// 25 中有2个5
-    	long count = 0;
-    	
-    	// 比如 n = 100 第一次可以分为5 x 20 
-    	// 在加上 20 = 5 x 4 中的4就可以
-    	long t = 5;
-    	while(t <= n){
-    		count += n / t;
-    		n /= 5;
-    	} 	
-    	return count;
+    public long trailingZeros(long n) {
+        // 只要统计5的个数就行了
+        long count = 0;
+        while (n > 0) {
+            count += n / 5;
+            n /= 5;
+        }
+        return count;
     }
 }
