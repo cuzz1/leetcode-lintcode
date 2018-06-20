f1213f94e047f6f0f8917e738404c94d3d620e18
cuzz1 <cuzz1234@163.com>
2018-06-20 15:26:51 +0800

    452

--- src/Solution452.java
+++ src/Solution452.java
@@ -20,22 +20,12 @@ public class Solution452 {
      * @param val: An integer
      * @return: a ListNode
      */
+	// 递归版本
     public ListNode removeElements(ListNode head, int val) {
-        ListNode dummy = new ListNode(0);
-        
-        dummy.next = head;
-        
-        ListNode pre = dummy;
-        while(pre.next != null) {
-            if (pre.next.val == val) {
-                ListNode node = pre.next;
-                pre.next = node.next;
-                node.next = null;
-            } else {
-                pre = pre.next;
-            }
-            
-        }
-        return dummy.next;
-    }
+    	if (head == null) {
+    		return null;
+    	}
+    	head.next = removeElements(head.next, val);
+    	return head.val == val ? head.next : head;
+    }  
 }
