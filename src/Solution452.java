b10939c6350e39238b82f8899310fe7ac6b81559
cuzz1 <cuzz1234@163.com>
2018-06-20 11:45:40 +0800

    452

--- src/Solution452.java
+++ src/Solution452.java
@@ -2,6 +2,18 @@ package src;
 
 import extend.ListNode;
 
+/**
+ * Definition for ListNode
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+
 public class Solution452 {
     /**
      * @param head: a ListNode
@@ -9,19 +21,21 @@ public class Solution452 {
      * @return: a ListNode
      */
     public ListNode removeElements(ListNode head, int val) {
-    	// write your code here
-    	// 在前面新增一个节点  这样不用考虑第一位子和最后一个位子
-    	ListNode pre = new ListNode(0);
-    	pre.next = head;
-    	head = pre;
-    	
-    	while(head.next != null){
-    		if(head.next.val == val){
-    			head.next = head.next.next;
-    		}else{
-    			head = head.next;
-    		}
-    	}
-    	return pre.next;
+        ListNode dummy = new ListNode(0);
+        
+        dummy.next = head;
+        
+        ListNode pre = dummy;
+        while(pre.next != null) {
+            if (pre.next.val == val) {
+                ListNode node = pre.next;
+                pre.next = node.next;
+                node.next = null;
+            } else {
+                pre = pre.next;
+            }
+            
+        }
+        return dummy.next;
     }
 }
