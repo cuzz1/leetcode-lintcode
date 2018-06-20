cc4e4c6d5e6b6844fd9d7a254804ad03fbe31f42
cuzz1 <cuzz1234@163.com>
2018-06-20 19:40:58 +0800

    539

--- src/Solution539.java
+++ src/Solution539.java
@@ -1,23 +1,24 @@
 package src;
 
-public class Solution539 {
+public class Solution {
     /**
      * @param nums: an integer array
      * @return: nothing
      */
     public void moveZeroes(int[] nums) {
-        int i = 0;
-        int count = 0;
-        while(i < nums.length - count) {
-            if (nums[i] == 0) {
-                for (int j = i; j < nums.length - 1; j++) {
-                    nums[j] = nums[j + 1];
-                }
-                count ++;
-                nums[nums.length - 1] = 0;
-            } else {
-                i++;
+        int p = 0;
+        
+        // 把非0元素从p位置开始放
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                nums[p] = nums[i];
+                p++;
             }
         }
+        // 最后的补0
+        for (int j = p; j < nums.length; j++) {
+            nums[j] = 0;
+        }
     }
+}
 }
