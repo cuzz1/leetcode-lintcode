09a14a27d955a75fe8ac074a5eb660801baacce4
cuzz1 <cuzz1234@163.com>
2018-06-27 12:34:30 +0800

    035

--- src/Solution035.java
+++ src/Solution035.java
@@ -2,21 +2,41 @@ package src;
 
 import extend.ListNode;
 
+/**
+ * Definition for ListNode
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+
 public class Solution035 {
     /**
      * @param head: n
      * @return: The new head of reversed linked list.
      */
     public ListNode reverse(ListNode head) {
-        // write your code here
-    	ListNode reversion = null;
-    	while(head != null){
-    		ListNode temp = head.next;
-    		head.next = reversion;
-    		
-    		reversion = head;
-    		head = temp;
-    	}
-    	return reversion;
+        ListNode node = null;
+        
+        while (head != null) {
+            ListNode temp = head;
+            
+            head = head.next;
+            
+            temp.next = node;
+            node = temp;
+        }
+        return node;
+        
     }
 }
+
+
+
+
+
+
