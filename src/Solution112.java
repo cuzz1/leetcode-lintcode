ccc724a7374cc82e78df7406c6dfffb0561c533f
cuzz1 <cuzz1234@163.com>
2018-06-28 11:29:12 +0800

    112

--- src/Solution112.java
+++ src/Solution112.java
@@ -2,28 +2,47 @@ package src;
 
 import extend.ListNode;
 
+/**
+ * Definition for ListNode
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+
 public class Solution112 {
     /**
      * @param head: head is the head of the linked list
      * @return: head of linked list
      */
     public ListNode deleteDuplicates(ListNode head) {
-    	
-    	if (head == null) {
-    		return null;
-    	}
-    	
-    	// 定义一个指针
-    	ListNode p= head;
-    	
-    	// 循环链表
-    	while (p.next != null) {
-    		if (p.val == p.next.val) {
-    			p.next = p.next.next;
-    		} else {
-    			p = p.next;
-    		}
-    	}
-    	return head;
+        // write your code here
+        if (head == null) {
+            return head;
+        }
+        
+        // 定义一个指针
+        ListNode p = head;
+        
+        while (p.next != null) {
+            if (p.val == p.next.val) {
+                p.next = p.next.next;
+            } else {
+                p = p.next;
+            }
+            
+        }
+        
+        return head;
     }
 }
+
+
+
+
+
+
