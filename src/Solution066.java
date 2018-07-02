30e2b7c5c96dbee2953897bab6723e3704a7ed92
cuzz1 <cuzz1234@163.com>
2018-07-02 10:35:01 +0800

    066

--- src/Solution066.java
+++ src/Solution066.java
@@ -43,29 +43,39 @@ public class Solution066 {
 }
 /**
  *  非递归版本
- *  public class Solution {
- 
-    List<Integer> list = new ArrayList<Integer>();
-    Stack<TreeNode> stack = new Stack<TreeNode>();
-    
+
+
+public class Solution {
+  
     public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        
+        Stack<TreeNode> stack = new Stack<>();
+        
         if (root == null) {
             return list;
         }
-        
         stack.push(root);
-        while(!stack.isEmpty()) {
-            TreeNode cur = stack.pop();
-            list.add(cur.val);
-            if (cur.right != null) {
-                stack.push(cur.right);
+        
+        while (!stack.isEmpty()) {
+            TreeNode node = stack.pop();
+            list.add(node.val);
+            
+            if (node.right != null) {
+                stack.push(node.right);
             }
-            if(cur.left != null) {
-                stack.push(cur.left);
-            } 
+            
+            if (node.left != null) {
+                stack.push(node.left);
+            }
+            
         }
         return list;
-        
     }
 }
+
+
+
+
+
  */
