1d57a1bd462f509fe732045c688fc252a319b5d8
cuzz1 <cuzz1234@163.com>
2018-07-10 11:01:10 +0800

    1317

--- src/SolutionI317.java
+++ src/SolutionI317.java
@@ -1,5 +1,29 @@
 package src;
 
-public class SolutionI317 {
+import extend.TreeNode;
 
+/**
+ * Definition of TreeNode:
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left, right;
+ *     public TreeNode(int val) {
+ *         this.val = val;
+ *         this.left = this.right = null;
+ *     }
+ * }
+ */
+
+public class SolutionI317 {
+    /**
+     * @param root: root of complete binary tree
+     * @return: the number of nodes
+     */
+    public int countNodes(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        
+        return countNodes(root.left) + countNodes(root.right) + 1;
+    }
 }
