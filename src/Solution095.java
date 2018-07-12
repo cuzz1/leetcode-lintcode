99207a23b0472dc4babd419cff3933665fbdfa46
cuzz1 <cuzz1234@163.com>
2018-07-12 10:36:57 +0800

    095

---
+++ src/Solution095.java
@@ -0,0 +1,50 @@
+package src;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import extend.TreeNode;
+
+/**
+ * Definition of TreeNode:
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left, right;
+ *     public TreeNode(int val) {
+ *         this.val = val;
+ *         this.left = this.right = null;
+ *     }
+ * }
+ */
+
+public class Solution095 {
+    /**
+     * @param root: The root of binary tree.
+     * @return: True if the binary tree is BST, or false
+     */
+    public boolean isValidBST(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        if (root.left == null && root.right == null) {
+            return true;
+        }
+        List<Integer> list = new ArrayList<Integer>();
+        inOrder(root, list);
+        for (int i = 1; i < list.size(); i++) {
+            if (list.get(i-1) >= list.get(i)) {
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    private void inOrder(TreeNode root, List<Integer> list) {
+        if (root == null) {
+            return;
+        }
+        inOrder(root.left, list);
+        list.add(root.val);
+        inOrder(root.right, list);
+    }
+}
