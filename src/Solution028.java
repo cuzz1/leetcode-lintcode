526d60f38a4550b68e0a960ef3e3ce1af2342b73
cuzz1 <cuzz1234@163.com>
2018-07-17 23:19:25 +0800

    28. 搜索二维矩阵

--- src/Solution028.java
+++ src/Solution028.java
@@ -24,3 +24,32 @@ public class Solution028 {
     	return false;
     }
 }
+//public class Solution {
+//    /**
+//     * @param matrix: matrix, a list of lists of integers
+//     * @param target: An integer
+//     * @return: a boolean, indicate whether matrix contains target
+//     */
+//    public boolean searchMatrix(int[][] matrix, int target) {
+//        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+//            return false;
+//        }
+//        int row = matrix.length;
+//        int column = matrix[0].length;
+//        int i;
+//        for (i = 0; i < row; i++) {
+//            if (target <= matrix[i][column - 1]) {
+//                break;
+//            }
+//        }
+//        if (i == row) {
+//            return false;
+//        }
+//        for (int j = 0; j < column; j ++) {
+//            if (matrix[i][j] == target) {
+//                return true;
+//            }
+//        }
+//        return false;
+//    }
+//}
