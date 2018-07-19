e04ed6c7c04bdc47527f71843e0f8f2cd010603f
cuzz1 <cuzz1234@163.com>
2018-07-19 19:28:39 +0800

    212. 空格替换

--- src/Solution212.java
+++ src/Solution212.java
@@ -1,35 +1,31 @@
 package src;
 
 public class Solution212 {
-    /*
-     * @param string: An array of Char
-     * @param length: The true length of the string
-     * @return: The true length of new string
-     */
     public int replaceBlank(char[] string, int length) {
         // write your code here
-    	int newLength = length;
-    	int space = 0;
-    	for (int i = 0; i < length; i++) {
-    		if (string[i] == ' ') {
-    			space++;
-    		}
-    	}
-    	
-    	newLength = newLength + space * 2;
-    	
-    	int index = newLength;
-    	
-    	// 从最后开始循环
-    	for (int i = length -1; i >= 0; i--) {
-    		if (string[i] == ' ') {
-    			string[--index] = '0';
-    			string[--index] = '2';
-    			string[--index] = '%';
-    		} else {
-    			string[--index] = string[i];
-    		}
-    	}
-    	return newLength;
+        if (string == null || length == 0) {
+            return 0;
+        }
+        int blankLength = 0;
+        for (int i = 0; i < string.length; i++) {
+            if (string[i] == ' ') {
+                blankLength ++;
+            }
+        }
+        
+        int size = length + blankLength * 2;
+        int j = size - 1;
+        
+        for (int i = length - 1; i >= 0; i--) {
+            if (string[i] == ' ') {
+                string[j--] = '0';
+                string[j--] = '2';
+                string[j--] = '%';
+            } else {
+                string[j--] = string[i];
+            }
+        }
+        return size;
+
     }
 }
