0ca5a3c2cc301bd57428ec9e0242b1c61295e08a
cuzz <32607900+cuzz1@users.noreply.github.com>
2018-08-06 14:35:09 +0800

    245. 子树

---
+++ src/Solution245.java
@@ -0,0 +1,52 @@
+/**
+ * Definition of TreeNode:
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left, right;
+ *     public TreeNode(int val) {
+ *         this.val = val;
+ *         this.left = this.right = null;
+ *     }
+ * }
+ */
+
+public class Solution {
+    /**
+     * @param T1: The roots of binary tree T1.
+     * @param T2: The roots of binary tree T2.
+     * @return: True if T2 is a subtree of T1, or false.
+     */
+    public boolean isSubtree(TreeNode T1, TreeNode T2) {
+        if (T2 == null) {
+            return true;
+        }
+        
+        if (T1 == null) {
+            return false;
+        }
+        
+        if (isEqual(T1, T2)) {
+            return true;
+        }
+        
+        return isSubtree(T1.left, T2) || isSubtree(T1.right, T2);
+    }
+    
+    private boolean isEqual(TreeNode T1, TreeNode T2) {
+        if (T1 == null || T2 == null) {
+            return T1 == T2;
+        }
+        
+        if (T1.val != T2.val) {
+            return false;
+        }
+        
+        return isEqual(T1.left, T2.left) && isEqual(T1.right, T2.right);
+    }
+}
+
+
+
+
+
+
