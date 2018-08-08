7ad3d668c8be287ea30a610b10780808f3f0875f
cuzz <32607900+cuzz1@users.noreply.github.com>
2018-08-08 14:29:11 +0800

    165. 合并两个排序链表

--- src/Solution165.java
+++ src/Solution165.java
@@ -2,31 +2,87 @@ package src;
 
 import extend.ListNode;
 
-public class Solution165 {
+/**
+ * Definition for ListNode
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
 
+public class Solution {
+    /**
+     * @param l1: ListNode l1 is the head of the linked list
+     * @param l2: ListNode l2 is the head of the linked list
+     * @return: ListNode head of linked list
+     */
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-    	
-    	ListNode head = new ListNode(0);
-    	ListNode node = head;
-    	
-    	while (l1 != null && l2 !=null) {
-    		if (l1.val <= l2.val) {
-    			node.next = l1;
-    			l1 = l1.next;
-    		} else {
-    			node.next = l2;
-    			l2 = l2.next;
-    		}
-    	node = node.next;
-    	}
-    	if (l1 != null) {
-			node.next = l1;
-		}
-    	
-    	if (l2 != null) {
-    		node.next = l2;
-    	}
-    	return head.next;
+        if (l1 == null) {
+            return l2;
+        }
+        if (l2 == null) {
+            return l1;
+        }
+        
+        List<Integer> list1 = new ArrayList<>();
+        List<Integer> list2 = new ArrayList<>();
+        while (l1 != null) {
+            list1.add(l1.val);
+            l1 = l1.next;
+        }
+        while (l2 != null) {
+            list2.add(l2.val);
+            l2 = l2.next;
+        }
+        
+        List<Integer> list3 = new ArrayList<>();
+        int p = 0;
+        int q = 0;
+        while (p < list1.size() && q < list2.size()) {
+            if (list1.get(p) < list2.get(q)) {
+                list3.add(list1.get(p));
+                p++;
+            } else {
+                list3.add(list2.get(q));
+                q++;
+            }
+        }
+        
+        while (p < list1.size()) {
+            list3.add(list1.get(p));
+            p++;
+        }
+        
+        while (q < list2.size()) {
+            list3.add(list2.get(q));
+            q++;
+        }
+        
+        ListNode head = new ListNode(list3.get(0));
+        ListNode node = head;
+        for (int i = 1; i < list3.size(); i++) {
+            node.next = new ListNode(list3.get(i));
+            node= node.next;
+        }
+        return head;
     }
-    
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
