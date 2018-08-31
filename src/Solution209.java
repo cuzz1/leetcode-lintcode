d387b244834ab823b3774e79f441f5e45590a0bc
cuzz <32607900+cuzz1@users.noreply.github.com>
2018-08-31 22:59:10 +0800

    209. 第一个只出现一次的字符

--- src/Solution209.java
+++ src/Solution209.java
@@ -1,26 +1,23 @@
-package src;
-
-public class Solution209 {
+public class Solution {
     /**
      * @param str: str: the given string
      * @return: char: the first unique character in a given string
      */
     public char firstUniqChar(String str) {
-        // Write your code here
-    	if (str == null || "".equals(str)) {
-    		return Character.MIN_VALUE;
-    	}
-    	
-    	int[] record = new int[256];
-    	for (int i = 0; i < str.length(); i ++) {
-    		record[str.charAt(i)] ++;
-    	}
-    	
-    	for (int i = 0; i < str.length(); i++) {
-    		if (record[str.charAt(i)] == 1) {
-    			return str.charAt(i);
-    		}
-    	}
-		return Character.MIN_VALUE;
+        if (str == null || "".equals(str)) {
+            return '0';
+        }
+       char[] record = new char[256];
+       for (int i = 0; i < str.length(); i++) {
+           record[str.charAt(i)]++;
+       }
+       
+       for (int i = 0; i < str.length(); i++) {
+           if (record[str.charAt(i)] == 1) {
+               return str.charAt(i);
+           }
+       }
+       
+       return Character.MIN_VALUE;
     }
 }
