0e857ef8b19f07a7c52ef97735bbb83a1d2dc822
cuzz <32607900+cuzz1@users.noreply.github.com>
2018-09-03 22:58:10 +0800

    41. 最大子数组

--- src/Solution041.java
+++ src/Solution041.java
@@ -1,25 +1,22 @@
-package src;
-
-public class Solution041 {
+public class Solution {
     /**
      * @param nums: A list of integers
      * @return: A integer indicate the sum of max subarray
      */
-	public static void main(String[] args) {
-		int[] nums = {1, 3, -1, -2, 6,-4, 4};
-		int result = maxSubArray(nums);
-		System.out.println(result);
-	}
-    public static int maxSubArray(int[] nums) {
-        // write your code here
-        int temp = nums[0];
-        for(int i = 0; i < nums.length; i++){
-        	int addedNums = 0;
-        	for(int j = i; j < nums.length; j++){
-        		addedNums += nums[j];
-        		temp = Math.max(temp, addedNums);
-        	}
+    public int maxSubArray(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
         }
-        return temp;
+        
+        int sum = 0;
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            max = Math.max(max, sum);
+            if (sum < 0) {
+                sum = 0;
+            }
+        }
+        return max;
     }
 }
