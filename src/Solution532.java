bb25cda903159a3d9488217f1a19e9bf718a3875
cuzz1 <32607900+cuzz1@users.noreply.github.com>
2018-10-06 19:36:11 +0800

    532. 逆序对

---
+++ src/Solution532.java
@@ -0,0 +1,17 @@
+public class Solution {
+    /**
+     * @param A: an array
+     * @return: total of reverse pairs
+     */
+    public long reversePairs(int[] A) {
+        long count = 0;
+        for (int i = 0; i < A.length; i++) {
+            for (int j = i + 1; j < A.length; j++) {
+                if (A[i] > A[j]) {
+                    count ++;
+                }
+            }
+        }
+        return count;
+    }
+}
