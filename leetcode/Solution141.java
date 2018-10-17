2fb3bfe1fca13a535f458c61844a985a227d0ed9
cuzz1 <32607900+cuzz1@users.noreply.github.com>
2018-10-17 16:14:22 +0800

    Create Solution141.java

---
+++ leetcode/Solution141.java
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        
+        if (head == null) {
+            return false;
+        }
+        
+        ListNode slow = head;
+        ListNode fast = head.next; 
+        
+        while (slow != null && fast != null && fast.next != null) {
+            if (slow == fast) {
+                return true;
+            }
+            slow = slow.next;
+            fast = fast.next.next;
+
+        }
+        return false;
+    }
+}
