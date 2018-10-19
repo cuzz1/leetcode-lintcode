87b680f7e5e3a627fc6ac2ed7f900689b8ceb985
cuzz1 <32607900+cuzz1@users.noreply.github.com>
2018-10-19 13:23:41 +0800

    Create Solution347.java

---
+++ leetcode/Solution347.java
@@ -0,0 +1,26 @@
+class Solution {
+    public List<Integer> topKFrequent(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i : nums) {
+            map.put(i, map.getOrDefault(i, 0) + 1);
+        }
+        
+        List<Integer>[] buckets = new ArrayList[nums.length + 1];
+        
+        for (int key : map.keySet()) {
+            int frequency = map.get(key);
+            if (buckets[frequency] == null) {
+                buckets[frequency] = new ArrayList<>();
+            }
+            buckets[frequency].add(key);
+        }
+        
+        List<Integer> res = new ArrayList<>();
+        for (int i = buckets.length - 1; i >= 0 && res.size() < k; i--) {
+            if (buckets[i] != null) {
+                res.addAll(buckets[i]);
+            }
+        }
+        return res;
+    }
+}
