787d1c2537657099da32de85dec009b8fe759072
cuzz1 <32607900+cuzz1@users.noreply.github.com>
2018-10-28 12:38:46 +0800

    Update Solution014.java

--- leetcode/Solution014.java
+++ leetcode/Solution014.java
@@ -22,3 +22,22 @@ class Solution {
         
     }
 }
+
+// O(N)
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        if (strs == null || strs.length == 0) {
+            return "";
+        } 
+        
+        String res = strs[0];
+        
+        for (int i = 0; i < strs.length; i++) {
+            // 必须找到等于0的这个子序列
+            while (strs[i].indexOf(res) != 0) {
+                res = res.substring(0, res.length() - 1);
+            }
+        }
+        return res;
+    }
+}
