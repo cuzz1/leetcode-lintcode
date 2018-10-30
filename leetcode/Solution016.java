ca6dc928a2e2cdc120abf17128a933a9f0c14c88
cuzz1 <32607900+cuzz1@users.noreply.github.com>
2018-10-30 11:42:24 +0800

    Update Solution016.java

--- leetcode/Solution016.java
+++ leetcode/Solution016.java
@@ -35,3 +35,31 @@ class Solution {
         return res[0] + res[1] + res[2];
     }
 }
+
+// fast
+class Solution {
+    public int threeSumClosest(int[] nums, int target) {
+        int res = nums[0] + nums[1] + nums[nums.length - 1];
+        Arrays.sort(nums);
+        
+        for (int i = 0; i < nums.length -2; i++) {
+            int low = i + 1; 
+            int high = nums.length - 1;
+            while (low < high) {
+                int sum = nums[i] + nums[low] + nums[high];
+                if (sum == target) {
+                    return target;
+                } else if (sum < target) {
+                    low++;
+                } else {
+                    high--;
+                }
+                
+                if (Math.abs(sum - target) < Math.abs(res - target)) {
+                    res = sum;
+                }
+            }
+        }
+        return res;
+    }
+}
