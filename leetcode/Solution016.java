47c9609468ef9cad0a7b76d2d2d2714f72045c1b
cuzz1 <32607900+cuzz1@users.noreply.github.com>
2018-10-30 11:33:37 +0800

    Create Solution016.java

---
+++ leetcode/Solution016.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int threeSumClosest(int[] nums, int target) {
+        
+        Arrays.sort(nums);
+        
+        int [] res = new int[3];
+        int abs = Integer.MAX_VALUE;
+        
+        for (int i = 0; i < nums.length -2; i++) {
+            int low = i + 1; 
+            int high = nums.length - 1;
+            int twoSum = target - nums[i];
+            
+            while (low < high) {
+                
+                int tar = Math.abs(nums[i] + nums[low] + nums[high] - target);
+                if (tar < abs) {
+                    abs = tar;
+                    res[0] = nums[i];
+                    res[1] = nums[low];
+                    res[2] = nums[high];
+                }
+             
+                if (twoSum == nums[low] + nums[high] ) {
+                    return target;
+                } 
+                
+                if (twoSum < nums[low] + nums[high]) {
+                    high--;
+                } else {
+                    low++;
+                }
+            }
+        }
+        return res[0] + res[1] + res[2];
+    }
+}
