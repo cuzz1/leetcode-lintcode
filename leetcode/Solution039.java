cbb705b18c6d2a19c1253a6c33bf936120cd5caf
cuzz1 <cuzz1234@163.com>
2018-11-15 11:19:09 +0800

    Create Solution039.java

---
+++ leetcode/Solution039.java
@@ -0,0 +1,23 @@
+class Solution {
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (candidates == null || candidates.length == 0) return res;
+        helper(res, new ArrayList<Integer>(), candidates, target, 0);
+        return res;
+    }
+    
+    private void helper(List<List<Integer>> res, List<Integer> list, int[] candidates, int target, int start) {
+        if (target < 0) {
+            return;
+        } 
+        if (target == 0) {
+            res.add(new ArrayList(list));
+            return;
+        }
+        for (int i  = start; i < candidates.length; i++) {
+            list.add(candidates[i]);
+            helper(res, list, candidates, target - candidates[i], i);
+            list.remove(list.size() - 1);
+        }
+    }
+}
