4f3865fd92de573c4b21d77d3b63e598e48251c6
cuzz1 <cuzz1234@163.com>
2018-11-16 11:32:33 +0800

    Create Solution040.java

---
+++ leetcode/Solution040.java
@@ -0,0 +1,24 @@
+class Solution {
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (candidates == null || candidates.length == 0) return res;
+        Arrays.sort(candidates);
+        helper(res, new ArrayList<Integer>(), candidates, target, 0);
+        return res;
+    }
+    
+    private void helper(List<List<Integer>> res, List<Integer> list, int[] candidates, int target, int start) {
+        if (target < 0) return;
+        if (target == 0) {
+            res.add(new ArrayList<Integer>(list));
+            return;
+        }
+        for (int i = start; i < candidates.length; i++) {
+            // 去重
+            if (i > start && candidates[i] == candidates[i-1]) continue;
+            list.add(candidates[i]);
+            helper(res, list, candidates, target - candidates[i], i + 1);
+            list.remove(list.size() - 1);
+        }
+    }
+}
