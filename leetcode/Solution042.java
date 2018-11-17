4ff93d399626943eb4f24da7c769c8c00cc2904b
cuzz1 <cuzz1234@163.com>
2018-11-17 11:01:43 +0800

    Create Solution042.java

---
+++ leetcode/Solution042.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int trap(int[] height) {
+        int left = 0;
+        int right = height.length - 1;
+        int leftMax = 0;
+        int rightMax = 0;
+        int res = 0;
+        while (left < right) {
+            if (height[left] < height[right]) {
+                leftMax = Math.max(leftMax, height[left]);
+                res += leftMax - height[left];
+                left++;
+            } else {
+                rightMax = Math.max(rightMax, height[right]);
+                res += rightMax - height[right];
+                right--;
+            }
+        }
+        return res;
+    }
+}
