44b227ded298718180fd133c42b874fbbbf2b00f
cuzz1 <cuzz1234@163.com>
2018-11-22 15:51:40 +0800

    Create Solution050.java

---
+++ leetcode/Solution050.java
@@ -0,0 +1,19 @@
+class Solution {
+    public double myPow(double x, int n) {
+        if (n > 0) {
+            return pow(x, n);
+        } else {
+            return 1.0 / pow(x, n);
+        }
+    }
+    
+    private double pow(double x, int n) {
+        if (n == 0) return 1;
+        double y = pow(x, n / 2);
+        if (n % 2 == 0) {
+            return y * y;
+        } else {
+            return y * y * x;
+        }
+    }
+}
