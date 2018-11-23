7240df99c5700348f514af23f10d110df04b3151
cuzz1 <cuzz1234@163.com>
2018-11-23 19:11:51 +0800

    Update Solution130.java

--- lintcode/Solution130.java
+++ lintcode/Solution130.java
@@ -1,29 +1,25 @@
-package src;
-
-public class Solution130 {
+public class Solution {
     /*
      * @param A: Given an integer array
      * @return: nothing
      */
     public void heapify(int[] A) {
-        int i;
-        for (i = 0; i < A.length; i++) {
-            swim(A, i);
+        
+        for (int i = 0; i < A.length; i++) {
+            smallToBig(A, i);
+        } 
+    }
+    
+    private void smallToBig(int[] A, int i) {
+        while(i > 0 && A[i] < A[(i-1)/2]) {
+            swap(A, i, (i-1) / 2);
+            i = (i-1) / 2;
         }
     }
     
-    private void swim(int[] A, int k) {
-        while (k > 0 && A[(k-1) / 2] > A[k]) {
-            exch(A, (k-1) / 2, k);
-            k = (k - 1) / 2;
-        }
-    }
-    
-    private void exch(int[] A, int i, int j) {
+    private void swap(int[] A, int i, int j) {
         int temp = A[i];
         A[i] = A[j];
         A[j] = temp;
     }
-    
-
 }
