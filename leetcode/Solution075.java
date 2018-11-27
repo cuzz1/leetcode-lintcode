5b99f5d01cbcf26887c8a3de7e4042ab48b03c48
cuzz1 <cuzz1234@163.com>
2018-11-27 20:08:23 +0800

    Update Solution075.java

--- leetcode/Solution075.java
+++ leetcode/Solution075.java
@@ -1,26 +1,31 @@
 class Solution {
     public void sortColors(int[] nums) {
+        int low = -1;
+        int high = nums.length;
         
-        int left= -1;
-        int right = nums.length;
-
-        
-        for (int i = 0; i < right;)  {
-            int temp = nums[i];
-            if (temp == 0) {
-                swap(nums, i++, ++left);
-            } else if (temp == 2) {
-                swap(nums, i , --right);
+        for (int i = 0; i < high; ) {
+            if (nums[i] == 0) {
+                swap(nums, i++, ++low);
+            } else if (nums[i] == 2) {
+                swap(nums, i, --high);
             } else {
                 i++;
             }
-            
+            // print(nums);
         }
+        
     }
     
     private void swap(int[] nums, int i, int j) {
         int temp = nums[i];
         nums[i] = nums[j];
         nums[j] = temp;
+    }
+    
+    private void print(int[] nums) {
+        for (int num : nums) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
     }
 }
