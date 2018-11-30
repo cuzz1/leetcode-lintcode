f4ce65fbdc03afb4266536159f8bd405538c0aee
cuzz1 <cuzz1234@163.com>
2018-11-30 21:01:55 +0800

    Update Solution366.java

--- lintcode/Solution366.java
+++ lintcode/Solution366.java
@@ -1,32 +1,17 @@
-package src;
-
-public class Solution366 {
+public class Solution {
     /**
      * @param n: an integer
      * @return: an ineger f(n)
      */
-    // public int fibonacci(int n) {
-    //     if (n == 1) {
-    //         return 0;
-    //     }
-    //     if (n == 2)  {
-    //         return 1;
-    //     }
-        
-    //     return fibonacci(n - 1) + fibonacci(n - 2);
-    // }
-    
     public int fibonacci(int n) {
-        int res = 0;
-        int i = 0;
-        int j = 1;
-        while (n > 1) {
-            int temp = i;
-            i = j;
-            j = temp + j;
-            n --;
+        
+        int a = 0;
+        int b = 1;
+        for (int i = 1; i < n; i++) {
+            int temp = a + b;
+            a = b;
+            b = temp;
         }
-        return i;
+        return a;
     }
-    
 }
