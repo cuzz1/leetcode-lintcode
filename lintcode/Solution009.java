d2bcf6b0ab6756bfe37260475b0d969343c09c05
cuzz1 <cuzz1234@163.com>
2018-11-30 20:55:45 +0800

    Update Solution009.java

--- lintcode/Solution009.java
+++ lintcode/Solution009.java
@@ -1,30 +1,26 @@
-package src;
-import java.util.ArrayList;
-import java.util.List;
-
-public class Solution009 {
+public class Solution {
     /**
      * @param n: An integer
      * @return: A list of strings.
      */
     public List<String> fizzBuzz(int n) {
-        List<String> list = new ArrayList<>();
+        final String FIZZ = "fizz";
+        final String BUZZ = "buzz";
+        final String FIZZ_BUZZ = "fizz buzz";
         
-        if (n <= 0) {
-            return list;
-        }
+        List<String> res = new ArrayList<>();
         
         for (int i = 1; i <= n; i++) {
-            if (i % 3 != 0 && i % 5 != 0) {
-                list.add(i + "");
-            } else if (i % 3 == 0 && i % 5 != 0) {
-                list.add("fizz");
-            } else if (i % 5 == 0 && i % 3 != 0) {
-                list.add("buzz");
-            }else {
-                list.add("fizz buzz");
+            if (i % 3 == 0 && i % 5 == 0) {
+                res.add(FIZZ_BUZZ);
+            } else if (i % 3 == 0) {
+                res.add(FIZZ);
+            } else if (i % 5 == 0) {
+                res.add(BUZZ);
+            } else {
+                res.add(String.valueOf(i));
             }
         }
-        return list;
+        return res;
     }
 }
