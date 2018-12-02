2b38912454d95a54a4e4a1844bdbbdeaaf4a5615
cuzz1 <cuzz1234@163.com>
2018-12-02 15:38:34 +0800

    Update Solution073.java

--- lintcode/Solution073.java
+++ lintcode/Solution073.java
@@ -1,7 +1,3 @@
-package src;
-
-import extend.TreeNode;
-
 /**
  * Definition of TreeNode:
  * public class TreeNode {
@@ -14,39 +10,25 @@ import extend.TreeNode;
  * }
  */
 
-public class Solution073 {
+public class Solution {
     /**
      * @param inorder: A list of integers that inorder traversal of a tree
      * @param postorder: A list of integers that postorder traversal of a tree
      * @return: Root of a tree
      */
     public TreeNode buildTree(int[] preorder, int[] inorder) {
-        TreeNode root = buildTree(preorder, 0, preorder.length - 1,
-                                  inorder, 0, inorder.length - 1);
-        return root; 
+        return buildTree(preorder, 0, preorder.length - 1, inorder, 0, inorder.length - 1);
     }
     
-    private TreeNode buildTree(int[] preorder, int startPre, int endPre,
-                               int[] inorder, int startIn, int endIn) {
-                                 
-        if (startPre > endPre || startIn > endIn) {
-            return null;
-        }
-        
-        // 根节点
-        TreeNode root = new TreeNode(preorder[startPre]);
-        
-        for (int i = startIn; i <= endIn; i++) {
-            if (inorder[i] == preorder[startPre]) {
-                root.left = buildTree(preorder, startPre + 1, startPre + i - startIn,
-                                     inorder, startIn, i - 1);
-                root.right = buildTree(preorder, startPre + i - startIn + 1, endPre,
-                                        inorder, i + 1, endIn);
+    private TreeNode buildTree(int[] preorder, int start1, int end1, int[] inorder, int start2, int end2) {
+        if (start1 > end1 || start2 > end2) return null;
+        TreeNode root = new TreeNode(preorder[start1]);
+        for (int i = start2; i <= end2; i++) {
+            if (inorder[i] == preorder[start1]) {
+                root.left = buildTree(preorder, start1 + 1, start1 + i - start2, inorder, start2, i - 1);
+                root.right = buildTree(preorder, start1 + i - start2 + 1, end1, inorder, i + 1, end2);
             }
         }
         return root;
     }
-    
-    
-    
 }
