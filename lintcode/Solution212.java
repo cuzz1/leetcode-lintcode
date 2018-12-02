fae2010d94fcf87774d79f345dae146208987aac
cuzz1 <cuzz1234@163.com>
2018-12-02 11:26:01 +0800

    Update Solution212.java

--- lintcode/Solution212.java
+++ lintcode/Solution212.java
@@ -1,31 +1,28 @@
-package src;
-
-public class Solution212 {
+public class Solution {
+    /*
+     * @param string: An array of Char
+     * @param length: The true length of the string
+     * @return: The true length of new string
+     */
     public int replaceBlank(char[] string, int length) {
-        // write your code here
-        if (string == null || length == 0) {
-            return 0;
-        }
-        int blankLength = 0;
-        for (int i = 0; i < string.length; i++) {
-            if (string[i] == ' ') {
-                blankLength ++;
-            }
+        int blankCount = 0;
+        for (int i = 0; i < length; i++) {
+           if (string[i] == ' ') {
+               blankCount++;
+           } 
         }
         
-        int size = length + blankLength * 2;
-        int j = size - 1;
-        
+        int newLength = length + blankCount * 2;
+        int index = newLength - 1;
         for (int i = length - 1; i >= 0; i--) {
             if (string[i] == ' ') {
-                string[j--] = '0';
-                string[j--] = '2';
-                string[j--] = '%';
+                string[index--] = '0';
+                string[index--] = '2';
+                string[index--] = '%';
             } else {
-                string[j--] = string[i];
+                string[index--] = string[i];
             }
         }
-        return size;
-
+        return newLength;
     }
 }
