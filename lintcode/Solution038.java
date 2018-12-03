f7137228a76d7ff14208c335f731b569aa63dc4f
cuzz1 <cuzz1234@163.com>
2018-12-03 11:58:30 +0800

    Update Solution038.java

--- lintcode/Solution038.java
+++ lintcode/Solution038.java
@@ -1,35 +1,27 @@
-package src;
-
-public class Solution038 {
+public class Solution {
     /**
      * @param matrix: A list of lists of integers
      * @param target: An integer you want to search in matrix
      * @return: An integer indicate the total occurrence of target in the given matrix
      */
     public int searchMatrix(int[][] matrix, int target) {
-        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
-            return 0;
-        }
-        
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) return 0;
+        int row = 0;
+        int col = matrix[0].length - 1;
         int count = 0;
         
-        int row = matrix.length - 1;
-        int column = matrix[0].length - 1;
-        
-        int i = 0;
-        int j = column;
-        
-        while (i <= row && j >= 0) {
-            if (matrix[i][j] < target) {
-                i++;
-            } else if (matrix[i][j] > target) {
-                j--;
-            } else {
+        while (row < matrix.length && col >= 0) {
+            // System.out.println(row + " " + col);
+            if (matrix[row][col] == target) {
                 count++;
-                i++;
-                j--;
+                row++;
+                col--;
+            } else if (matrix[row][col] > target) {
+                col--;
+            } else {
+                row++;
             }
         }
-    return count;
+        return count;
     }
 }
