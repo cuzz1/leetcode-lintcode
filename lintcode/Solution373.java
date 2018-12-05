dcae3ff7bd8f5f73cdad8179ca28c87ce17d70de
cuzz1 <cuzz1234@163.com>
2018-12-05 11:03:02 +0800

    Update Solution373.java

--- lintcode/Solution373.java
+++ lintcode/Solution373.java
@@ -1,4 +1,23 @@
-package src;
-
-
-
+public class Solution {
+    /*
+     * @param nums: an array of integers
+     * @return: nothing
+     */
+    public void partitionArray(int[] nums) {
+        
+        if (nums == null || nums.length == 0) return;
+        
+        int p = -1;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] % 2 != 0) {
+                swap(nums, ++p, i);
+            }
+        }
+    }
+    
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
