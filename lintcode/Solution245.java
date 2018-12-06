26d96d9c1916b71ee804df4953700b649f8b4a3f
cuzz1 <cuzz1234@163.com>
2018-12-06 11:55:01 +0800

    Update Solution245.java

--- lintcode/Solution245.java
+++ lintcode/Solution245.java
@@ -17,36 +17,15 @@ public class Solution {
      * @return: True if T2 is a subtree of T1, or false.
      */
     public boolean isSubtree(TreeNode T1, TreeNode T2) {
-        if (T2 == null) {
-            return true;
-        }
-        
-        if (T1 == null) {
-            return false;
-        }
-        
-        if (isEqual(T1, T2)) {
-            return true;
-        }
-        
+        if (T2 == null) return true;
+        if (T1 == null) return false;
+        if (isSame(T1, T2)) return true;
         return isSubtree(T1.left, T2) || isSubtree(T1.right, T2);
     }
     
-    private boolean isEqual(TreeNode T1, TreeNode T2) {
-        if (T1 == null || T2 == null) {
-            return T1 == T2;
-        }
-        
-        if (T1.val != T2.val) {
-            return false;
-        }
-        
-        return isEqual(T1.left, T2.left) && isEqual(T1.right, T2.right);
+    private boolean isSame(TreeNode T1, TreeNode T2) {
+        if (T1 == null || T2 == null) return T1 == T2;
+        if (T1.val != T2.val) return false;
+        return isSame(T1.right, T2.right) && isSame(T1.left, T2.left);
     }
 }
-
-
-
-
-
-
