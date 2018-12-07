67d088b36fc68c53dad12919b7dc2fa87c13facb
cuzz1 <cuzz1234@163.com>
2018-12-07 14:25:15 +0800

    Update Solution211.java

--- lintcode/Solution211.java
+++ lintcode/Solution211.java
@@ -5,21 +5,15 @@ public class Solution {
      * @return: a boolean
      */
     public boolean Permutation(String A, String B) {
-        char[] A_array = new char[256];
-        char[] B_array = new char[256];
-        if (A.length() != B.length()) {
-            return false;
+        char[] array1 = new char[256];
+        char[] array2 = new char[256];
+        if (A.length() != B.length()) return false;
+        for (int i = 0; i < A.length(); i++) {
+            array1[A.charAt(i)]++;
+            array2[B.charAt(i)]++;
         }
-        
-        for (int i = 0; i < A.length(); i++) { 
-            A_array[A.charAt(i)]++;
-            B_array[B.charAt(i)]++;
-        }
-        
-        for (int i = 0; i < A_array.length; i++) {
-            if (A_array[i] != B_array[i]) {
-                return false;
-            }
+        for (int i = 0; i < array1.length; i++) {
+            if (array1[i] != array2[i]) return false;
         }
         return true;
     }
