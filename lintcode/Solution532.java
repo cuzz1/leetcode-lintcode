07019740838168ed4721ccbc5ef4461e12aa4e80
cuzz1 <cuzz1234@163.com>
2018-12-11 15:03:59 +0800

    Update Solution532.java

--- lintcode/Solution532.java
+++ lintcode/Solution532.java
@@ -5,11 +5,10 @@ public class Solution {
      */
     public long reversePairs(int[] A) {
         long count = 0;
+        if (A == null || A.length == 0) return count;
         for (int i = 0; i < A.length; i++) {
             for (int j = i + 1; j < A.length; j++) {
-                if (A[i] > A[j]) {
-                    count ++;
-                }
+                if (A[i] > A[j]) count++;
             }
         }
         return count;
