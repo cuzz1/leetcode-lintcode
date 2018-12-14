03e885de2dc46e3c766de27233b8769914f6bfea
cuzz1 <cuzz1234@163.com>
2018-12-14 19:48:52 +0800

    Update Solution003.java

--- lintcode/Solution003.java
+++ lintcode/Solution003.java
@@ -1,35 +1,26 @@
-package src;
-
-public class Solution003 {
-    /*
-     * @param : An integer
-     * @param : An integer
+public class Solution {
+    /**
+     * @param k: An integer
+     * @param n: An integer
      * @return: An integer denote the count of digit k in 1..n
      */
-	public static void main(String[] args) {
-		
-		int count = digitCounts(9, 99999);
-		System.out.println(count);
-	}
-    public static int digitCounts(int k, int n) {
-        // write your code here
-    	int count = 0;
-    	for (int i = 0; i <= n; i++) {
-    		count += singleCount(k, i);
-    	}
-    	return count;
+    public int digitCounts(int k, int n) {
+        int count = 0;
+        for(int i = 0; i <= n; i++) {
+            count += helper(k, i);
+        }
+        return count;
     }
-	private static int singleCount(int k, int i) {
-		if (i == 0 && k == 0) {
-			return 1;
-		}
-		int count = 0;
-		while (i > 0) {
-			if (i % 10 == k) {
-				count++;
-			}
-			i = i / 10;
-		}
-		return count;
-	}
+    
+    private int helper(int k, int i) {
+        if (k == 0 && i == 0) return 1;
+        int count = 0;
+        while (i > 0) {
+            if (i % 10 == k) {
+                count++;
+            }
+            i /= 10;
+        }
+        return count;
+    }
 }
