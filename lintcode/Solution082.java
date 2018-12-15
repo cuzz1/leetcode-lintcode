576e70ba073ea65a6c431e76451b7785922de353
cuzz1 <cuzz1234@163.com>
2018-12-15 13:43:31 +0800

    Update Solution082.java

--- lintcode/Solution082.java
+++ lintcode/Solution082.java
@@ -1,33 +1,10 @@
-package src;
-
-import java.util.Map;
-import java.util.HashMap;
-import java.util.Set;
-
-public class Solution082 {
-    /**
-     * @param A: An integer array
-     * @return: An integer
-     */
+public class Solution {
+    // a ^ b ^ b = a
     public int singleNumber(int[] A) {
-        Map<Integer, Integer> dict = new HashMap<Integer, Integer>();
-        
-        for (int a : A) {
-        	if (dict.containsKey(a)){
-        		int value = dict.get(a) + 1;
-        		dict.put(a, value);
-        	} else {
-        		dict.put(a, 1);
-        	}
+        int res = A[0];
+        for (int i = 1; i < A.length; i++) {
+            res ^= A[i];
         }
-        
-        Set<Integer> keySet = dict.keySet();
-        for (int key : keySet) {
-        	int value = dict.get(key);
-        	if (value == 1) {
-        		return key;
-        	}
-        }
-		return 0;
+        return res;
     }
 }
