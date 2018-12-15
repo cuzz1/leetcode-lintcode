8ce373b494cc03a8b8f9e1c52a9ab35369654dbc
cuzz1 <cuzz1234@163.com>
2018-12-15 14:07:48 +0800

    Update Solution056.java

--- lintcode/Solution056.java
+++ lintcode/Solution056.java
@@ -1,56 +1,21 @@
-package src;
-
-public class Solution056 {
+public class Solution {
     /**
      * @param numbers: An array of Integer
      * @param target: target = numbers[index1] + numbers[index2]
-     * @return: [index1 + 1, index2 + 1] (index1 < index2)
+     * @return: [index1, index2] (index1 < index2)
      */
-	public static void main(String[] args) {
-		int[] numbers = {2, 7, 11, 15};
-		int[] result = twoSum(numbers, 3);
-		for(int i = 0; i < result.length; i++){
-			System.out.println(result[i]);
-		}
-	}
-    public static int[] twoSum(int[] numbers, int target) {
-        // write your code here
-    	int[] result = new int[2];
-    	for(int i = 0; i < numbers.length; i++){
-    		int temp = target - numbers[i];
-    		for(int j = i + 1; j < numbers.length; j++){
-    			if(temp == numbers[j]){
-    				result[0] = i;
-    				result[1] = j;
-    				return result;
-    			}
-    		}
-    	}
-		return result;
-    }
-}
-/*
- 
- public class Solution {
-  
-     // O(n)
     public int[] twoSum(int[] numbers, int target) {
-        int [] res = new int[2];
-        Map<Integer, Integer>  map = new HashMap<>(); 
+        
+        // Key 是数组的值 Value是数组的index
+        Map<Integer, Integer> map = new HashMap<>();
         
         for (int i = 0; i < numbers.length; i++) {
-            int temp = target - numbers[i];
-            if (map.containsKey(temp)) {
-                res[0] = map.get(temp);
-                res[1] = i;
-                return res;
-            }
-            map.put(numbers[i], i);
-            
+            if (map.containsKey(target-numbers[i])) {
+                return new int[]{map.get(target-numbers[i]), i};
+            } else {
+                map.put(numbers[i], i);
+            }  
         }
-        return res;
+        return new int[]{-1, -1};
     }
 }
- 
- 
- */
