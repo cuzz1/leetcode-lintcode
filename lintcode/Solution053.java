24638d2b24e1b903877b2bac474b48049aa982a9
cuzz1 <cuzz1234@163.com>
2018-12-15 14:28:41 +0800

    Update Solution053.java

--- lintcode/Solution053.java
+++ lintcode/Solution053.java
@@ -1,34 +1,25 @@
-package src;
-
-import java.util.ArrayList;
-import java.util.List;
-
-public class Solution053 {
+public class Solution {
     /*
      * @param s: A string
      * @return: A string
      */
-	public static void main(String[] args) {
-		String s = " the world is so big that i want to see it  ";
-		String str = reverseWords(s);
-		System.out.println(str);
-	}
-    public static String reverseWords(String s) {
-        // write your code here
-    	if (s.length() == 0 || s.equals(" ")){
-    		return "";
-    	}
-    	
-    	String[] array = s.split(" ");
-    	StringBuilder sb = new StringBuilder();
-    	for (int i = array.length - 1; i >= 0; i--){
-    		if (!array[i].equals(" ")){  			
-    			if(sb.length() > 0){
-    				sb.append(" ");
-    			}
-    			sb.append(array[i]);
-    		}
-    	}
-    	return sb.toString();
+    public String reverseWords(String s) {
+        String res = "";
+        String str = reverse(s.trim()); 
+        String[] strs = str.split("\\s+");
+        for (int i = 0; i < strs.length; i++) {
+            res += reverse(strs[i]) + " ";
+        }
+        return res.substring(0, res.length() - 1);
+    }
+    
+    private String reverse(String s) {
+        char[] chars = s.toCharArray();
+        for (int i = 0; i < chars.length / 2; i++) {
+            char temp = chars[i];
+            chars[i] = chars[chars.length - 1- i];
+            chars[chars.length - 1 - i] = temp;
+        }
+        return new String(chars);
     }
 }
