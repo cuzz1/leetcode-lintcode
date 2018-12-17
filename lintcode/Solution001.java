07aa92e589cfaca627f9e7e2297cd0ab49b2f14d
cuzz1 <cuzz1234@163.com>
2018-12-17 13:42:11 +0800

    Update Solution001.java

--- lintcode/Solution001.java
+++ lintcode/Solution001.java
@@ -1,18 +1,10 @@
-package src;
-
-public class Solution001 {
+public class Solution {
     /**
      * @param a: An integer
      * @param b: An integer
-     * @return: The sum of a and b
+     * @return: The sum of a and b 
      */
     public int aplusb(int a, int b) {
-    	while (b != 0) {
-    		int _a = a ^ b;
-    		int _b = (a & b) << 1;
-    		a = _a;
-    		b = _b;
-    	}
-    	return a;
+        return b == 0 ? a : aplusb(a ^ b, (a & b) << 1);
     }
 }
