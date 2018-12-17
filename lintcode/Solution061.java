652d160269756aa18b9731a9baab977a71baf157
cuzz1 <cuzz1234@163.com>
2018-12-17 14:26:31 +0800

    Update Solution061.java

--- lintcode/Solution061.java
+++ lintcode/Solution061.java
@@ -5,45 +5,37 @@ public class Solution {
      * @return: a list of length 2, [index1, index2]
      */
     public int[] searchRange(int[] A, int target) {
-        
-        int[] res =  new int[]{-1, -1};
-        
-        if (A == null || A.length == 0) {
-            return res;
-        }
-        
-        
+        if (A == null || A.length == 0) return new int[]{-1, -1};
+        return new int[]{findMin(A, target), findMax(A, target)};
+    }
+    
+    private int findMin(int[] A, int target) {
         int start = 0;
         int end = A.length - 1;
-        int mid = 0;
-        
-        
-        boolean flag = false;
-        while (start <= end ) {
-            mid = start + (end - start) / 2;
-            if (A[mid] < target) {
-                start = mid + 1;
-            } else if (A[mid] > target) {
-                end = mid -1;
+        while (start + 1 < end) {
+            int mid = start + (end - start) / 2;
+            if (A[mid] >= target) {
+                end = mid;
             } else {
-                flag = true;
-                break;
+                start = mid;
             }
         }
-        
-        if (flag == false) {
-            return res;
+        if (A[start] == target) return start;
+        return A[end] == target ? end : -1;
+    }
+    
+    private int findMax(int[] A, int target) {
+        int start = 0;
+        int end = A.length - 1;
+        while (start + 1 < end) {
+            int mid = start + (end - start) / 2;
+            if (A[mid] > target) {
+                end = mid;
+            } else {
+                start = mid;
+            }
         }
-        
-        int left = mid;
-        int right = mid;
-        
-        while (left >= 0 && A[left] == target) {
-            left--;
-        }
-        while (right < A.length && A[right] == target) {
-            right++;
-        }
-        return new int[]{left + 1, right - 1};
+        if (A[end] == target) return end;
+        return A[start] == target ? start : -1;
     }
 }
