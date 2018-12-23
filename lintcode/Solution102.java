13aaef6f94a16984685c26761343efb1e52c3ae7
cuzz1 <cuzz1234@163.com>
2018-12-23 15:49:37 +0800

    Update Solution102.java

--- lintcode/Solution102.java
+++ lintcode/Solution102.java
@@ -1,30 +1,28 @@
-package src;
+/**
+ * Definition for ListNode
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
 
-import extend.ListNode;
-
-public class Solution102 {
-
-    /* 定义两个指针一个每次移动两次一个移动一次 如果有环则他们会相遇
-     * 已经用python试过了
+public class Solution {
+    /**
      * @param head: The first node of linked list.
      * @return: True if it has a cycle, or false
      */
     public boolean hasCycle(ListNode head) {
-    	
-    	if (head == null || head.next == null) {
-    		return false;
-    	}
-    	
-    	ListNode fast = head.next;
-    	ListNode slow = head;
-    	while(fast != slow) {
-    		if (fast == null || fast.next == null) {
-    			return false;
-    		}
-    		fast = fast.next.next;
-    		slow = slow.next;
-    	}
-    	return true;
-
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) return true;
+        }
+        return false;
     }
 }
