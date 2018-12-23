516ea622cc233c0ed08b79440be505572a2c27f3
cuzz1 <cuzz1234@163.com>
2018-12-23 13:24:52 +0800

    Update Solution056.java

--- lintcode/Solution056.java
+++ lintcode/Solution056.java
@@ -5,16 +5,16 @@ public class Solution {
      * @return: [index1, index2] (index1 < index2)
      */
     public int[] twoSum(int[] numbers, int target) {
-        
-        // Key 是数组的值 Value是数组的index
+        if (numbers == null || numbers.length < 2) return new int[]{-1,-1};
+        // key是数，value是下标
         Map<Integer, Integer> map = new HashMap<>();
-        
         for (int i = 0; i < numbers.length; i++) {
-            if (map.containsKey(target-numbers[i])) {
-                return new int[]{map.get(target-numbers[i]), i};
-            } else {
+            int value = target - numbers[i];
+            if (!map.containsKey(value)) {
                 map.put(numbers[i], i);
-            }  
+            } else {
+                return new int[]{map.get(value), i};
+            }
         }
         return new int[]{-1, -1};
     }
