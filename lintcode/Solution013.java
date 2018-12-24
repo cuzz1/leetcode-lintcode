b570cd591d72fc7cfaa422c7d455bb446e41b955
cuzz1 <cuzz1234@163.com>
2018-12-24 10:42:37 +0800

    Update Solution013.java

--- lintcode/Solution013.java
+++ lintcode/Solution013.java
@@ -1,28 +1,20 @@
-package src;
-
-public class Solution013 {
-    /*
-     * @param source: source string to be scanned.
-     * @param target: target string containing the sequence of characters to match
-     * @return: a index to the first occurrence of target in source, or -1  if target is not part of source.
+public class Solution {
+    /**
+     * @param source: 
+     * @param target: 
+     * @return: return the index
      */
     public int strStr(String source, String target) {
-    	
-    	if(source == null || target == null){
-    		return -1;
-    	}
-    	
-    	for(int i = 0; i < source.length() - target.length() + 1; i++){
-    		int j;
-    		for(j = 0; j < target.length(); j++){
-    			if(source.charAt(i + j) != target.charAt(j)){
-    				break;
-    			}
-    		}
-    		if (j == target.length()){
-    			return i;
-    		}
-    	}
-    	return -1;
+        
+        for (int i = 0; i <= source.length() - target.length(); i++) {
+            int j = 0;
+            for (j = 0; j < target.length(); j++) {
+                if (source.charAt(i + j) != target.charAt(j)) {
+                    break;
+                }
+            }
+            if (j == target.length()) return i;
+        }
+        return -1;
     }
 }
