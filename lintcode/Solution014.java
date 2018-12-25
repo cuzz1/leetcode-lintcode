5f901caef1eaa000db479e660a9216ee7bc38237
cuzz1 <cuzz1234@163.com>
2018-12-25 11:26:51 +0800

    Update Solution014.java

--- lintcode/Solution014.java
+++ lintcode/Solution014.java
@@ -1,35 +1,26 @@
-package src;
-
-public class Solution014 {
-	/**
-	 * @param nums: The integer array.
-	 * @param target: Target to find.
-	 * @return: The first position of target. Position starts from 0.
-	 */
-	public int binarySearch(int[] nums, int target) {
-		int start = 0;
-		int end = nums.length - 1;
-		// 范围在 [start, end] 这个闭区间里
-		// 所以start == end [start, end] 仍然有效
-		while (start <= end) {
-			int mid = start + (end - start) / 2;
-			if (nums[mid] == target) {
-				
-				while (mid > 0 && nums[mid] == nums[mid - 1]) {
-					mid --;
-				}
-				return mid;
-			}
-			
-			if (nums[mid] > target) {
-				end = mid - 1;
-			}
-			
-			if (nums[mid] < target) {
-				start = mid + 1;
-			}
-		}
-		return -1;
-	}
+public class Solution {
+    /**
+     * @param nums: The integer array.
+     * @param target: Target to find.
+     * @return: The first position of target. Position starts from 0.
+     */
+    public int binarySearch(int[] nums, int target) {
+        if (nums == null || nums.length == 0) return -1;
+        int start = 0;
+        int end = nums.length - 1;
+        while (start + 1 < end) {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] == target) {
+                end = mid;
+            } else if (nums[mid] > target) {
+                end = mid;
+            } else {
+                start = mid;
+            }
+        }
+        if (nums[start] == target) return start;
+        if (nums[end] == target) return end;
+        return -1;
+    }
 }
  
