2be678e118343a24bebfc901206097a33df9508a
cuzz1 <cuzz1234@163.com>
2018-12-30 13:23:19 +0800

    Update Solution069.java

--- lintcode/Solution069.java
+++ lintcode/Solution069.java
@@ -1,15 +1,3 @@
-/**
- * Definition of TreeNode:
- * public class TreeNode {
- *     public int val;
- *     public TreeNode left, right;
- *     public TreeNode(int val) {
- *         this.val = val;
- *         this.left = this.right = null;
- *     }
- * }
- */
-
 public class Solution {
     /**
      * @param root: A Tree
@@ -17,17 +5,17 @@ public class Solution {
      */
     public List<List<Integer>> levelOrder(TreeNode root) {
         List<List<Integer>> res = new ArrayList<>();
-        Queue<TreeNode> queue = new LinkedList<>();
         if (root == null) return res;
-        queue.add(root);
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
         while (!queue.isEmpty()) {
             int size = queue.size();
-            List list = new ArrayList<>();
+            List<Integer> list = new ArrayList<>();
             while (size-- > 0) {
                 TreeNode node = queue.poll();
-                list.add(node.val);    
-                if (node.left != null) queue.add(node.left);
-                if (node.right != null) queue.add(node.right);
+                list.add(node.val);
+                if (node.left != null) queue.offer(node.left);
+                if (node.right != null) queue.offer(node.right);
             }
             res.add(list);
         }
