fd5b25213889e96ce4f9c045168032e2b14ce83d
cuzz1 <cuzz1234@163.com>
2018-12-30 13:44:20 +0800

    Update Solution095.java

--- lintcode/Solution095.java
+++ lintcode/Solution095.java
@@ -1,50 +1,15 @@
-package src;
-
-import java.util.ArrayList;
-import java.util.List;
-
-import extend.TreeNode;
-
-/**
- * Definition of TreeNode:
- * public class TreeNode {
- *     public int val;
- *     public TreeNode left, right;
- *     public TreeNode(int val) {
- *         this.val = val;
- *         this.left = this.right = null;
- *     }
- * }
- */
-
-public class Solution095 {
+public class Solution {
     /**
      * @param root: The root of binary tree.
      * @return: True if the binary tree is BST, or false
      */
     public boolean isValidBST(TreeNode root) {
-        if (root == null) {
-            return true;
-        }
-        if (root.left == null && root.right == null) {
-            return true;
-        }
-        List<Integer> list = new ArrayList<Integer>();
-        inOrder(root, list);
-        for (int i = 1; i < list.size(); i++) {
-            if (list.get(i-1) >= list.get(i)) {
-                return false;
-            }
-        }
-        return true;
+        return helper(root, Long.MIN_VALUE, Long.MAX_VALUE);
     }
     
-    private void inOrder(TreeNode root, List<Integer> list) {
-        if (root == null) {
-            return;
-        }
-        inOrder(root.left, list);
-        list.add(root.val);
-        inOrder(root.right, list);
+    private boolean helper(TreeNode root, long min, long max) {
+        if (root == null) return true;
+        if (root.val <= min || root.val >= max) return false;
+        return helper(root.left, min, Math.min(max, root.val)) && helper(root.right, Math.max(min, root.val), max);
     }
 }
