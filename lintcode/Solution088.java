96258d7810cfb4e61991d7a1e8dec0f591c58ef6
cuzz1 <cuzz1234@163.com>
2018-12-30 13:05:05 +0800

    Update Solution088.java

--- lintcode/Solution088.java
+++ lintcode/Solution088.java
@@ -1,30 +1,10 @@
-/**
- * Definition of TreeNode:
- * public class TreeNode {
- *     public int val;
- *     public TreeNode left, right;
- *     public TreeNode(int val) {
- *         this.val = val;
- *         this.left = this.right = null;
- *     }
- * }
- */
-
-
 public class Solution {
-    /*
-     * @param root: The root of the binary search tree.
-     * @param A: A TreeNode in a Binary.
-     * @param B: A TreeNode in a Binary.
-     * @return: Return the least common ancestor(LCA) of the two nodes.
-     */
+    // 分治的思想
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode A, TreeNode B) {
-        if (root == null || A == root || B == root) return root;
+        if (root == null || root == A || root == B) return root;
         TreeNode left = lowestCommonAncestor(root.left, A, B);
         TreeNode right = lowestCommonAncestor(root.right, A, B);
-        // 左边和右边各找到一个
-        if (left !=null && right != null) return root;
-        return left != null ? left : right;
+        if (left != null && right != null) return root;
+        return left == null ? right : left;
     }
 }
-
