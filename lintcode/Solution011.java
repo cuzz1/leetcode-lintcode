1a032480498eaad8c3b6273aa16aa2d21e414e8d
cuzz1 <cuzz1234@163.com>
2018-12-30 14:18:14 +0800

    Update Solution011.java

--- lintcode/Solution011.java
+++ lintcode/Solution011.java
@@ -1,49 +1,22 @@
-package src;
-
-import java.util.ArrayList;
-import java.util.List;
-
-import extend.TreeNode;
-
-/**
- * Definition of TreeNode:
- * public class TreeNode {
- *     public int val;
- *     public TreeNode left, right;
- *     public TreeNode(int val) {
- *         this.val = val;
- *         this.left = this.right = null;
- *     }
- * }
- */
-
-public class Solution011 {
+public class Solution {
     /**
      * @param root: param root: The root of the binary search tree
      * @param k1: An integer
      * @param k2: An integer
      * @return: return: Return all keys that k1<=key<=k2 in ascending order
      */
-    ArrayList<Integer> list = new ArrayList<Integer> ();
     public List<Integer> searchRange(TreeNode root, int k1, int k2) {
-        search(root, k1, k2);
+        List<Integer> list = new ArrayList<>();
+        helper(list, root, k1, k2);
         return list;
     }
     
-    private void search(TreeNode root, int k1, int k2) {
-        if (root == null) {
-            return;
-        }
-        
-        if (root.val > k1) {
-            search(root.left, k1, k2);
-        } 
-        if (root.val >= k1 && root.val <= k2) {
+    private void helper(List<Integer> list, TreeNode root, int k1, int k2) {
+        if (root == null) return;
+        if (k1 <= root.val && root.val <= k2) {
             list.add(root.val);
         }
-        if (root.val < k2) {
-            search(root.right, k1, k2);
-        } 
-        
+        helper(list, root.left, k1, k2);
+        helper(list, root.right, k1, k2);
     }
 }
