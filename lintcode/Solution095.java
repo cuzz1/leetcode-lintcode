58e721e38d97c3276d387baf46b197e99c35d10c
cuzz1 <cuzz1234@163.com>
2018-12-30 13:50:22 +0800

    Update Solution095.java

--- lintcode/Solution095.java
+++ lintcode/Solution095.java
@@ -13,3 +13,26 @@ public class Solution {
         return helper(root.left, min, Math.min(max, root.val)) && helper(root.right, Math.max(min, root.val), max);
     }
 }
+
+// 利用中序排序为递增
+public class Solution {
+    /**
+     * @param root: The root of binary tree.
+     * @return: True if the binary tree is BST, or false
+     */
+    public boolean isValidBST(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        inorder(root, list);
+        for (int i = 0; i < list.size() - 1; i++) {
+            if (list.get(i) >= list.get(i+1)) return false;
+        }
+        return true;
+    }
+    
+    private void inorder(TreeNode root, List<Integer> list) {
+        if (root == null) return;
+        inorder(root.left, list);
+        list.add(root.val);
+        inorder(root.right, list);
+    }
+}
