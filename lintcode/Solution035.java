8318565187314b31befefe215ceacee193451674
cuzz1 <cuzz1234@163.com>
2019-01-02 14:16:59 +0800

    Update Solution035.java

--- lintcode/Solution035.java
+++ lintcode/Solution035.java
@@ -1,50 +1,19 @@
-/**
- * Definition for ListNode
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode(int x) {
- *         val = x;
- *         next = null;
- *     }
- * }
- */
-
 public class Solution {
     /**
      * @param head: n
      * @return: The new head of reversed linked list.
      */
     public ListNode reverse(ListNode head) {
-        ListNode res = null;
-        
+        ListNode node = null;
         while (head != null) {
-            ListNode p = head.next;     
-            head.next = res;
-            res = head;
-            head = p;
+            ListNode next = head.next;
+            head.next = node;
+            node = head;
+            head = next;
         }
-        return res;
+        return node;
     }
 }
-
-
-// 递归
-public class Solution {
-    /**
-     * @param head: n
-     * @return: The new head of reversed linked list.
-     */
-    public ListNode reverse(ListNode head) {
-        if (head == null || head.next == null) return head;
-        // 1 null<-2<-3<-4  
-        ListNode newHead = reverse(head.next);
-        head.next.next = head;
-        head.next = null;
-        return newHead;
-    }
-}
-
 
 
 
