5d9b4fa4cc9bb6c5155078b367d0af807a93508b
cuzz1 <cuzz1234@163.com>
2019-01-03 11:20:33 +0800

    Create Solution098.java

---
+++ lintcode/Solution098.java
@@ -0,0 +1,46 @@
+public class Solution {
+    /**
+     * @param head: The head of linked list.
+     * @return: You should return the head of the sorted linked list, using constant space complexity.
+     */
+    public ListNode sortList(ListNode head) {
+        if (head == null || head.next == null) return head;
+        ListNode mid = findMid(head);
+        ListNode right = sortList(mid.next);
+        mid.next = null;
+        ListNode left = sortList(head);
+        return merge(left, right);
+    }
+    
+    private ListNode merge(ListNode head1, ListNode head2) {
+        ListNode dummy = new ListNode(0);
+        ListNode p = dummy;
+        while (head1 != null && head2 != null) {
+            if (head1.val < head2.val) {
+                p.next = head1;
+                head1 = head1.next;
+            } else {
+                p.next = head2;
+                head2 = head2.next;
+            }
+            p = p.next;
+        }
+        
+        if (head1 != null) {
+            p.next = head1;
+        } else {
+            p.next = head2;
+        }
+        return dummy.next;
+    }
+    
+    private ListNode findMid(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head.next;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+}
