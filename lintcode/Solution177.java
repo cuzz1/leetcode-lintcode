3f55a55f85204a9d9f47ac97a0301ec4bc2b164d
cuzz1 <cuzz1234@163.com>
2019-01-05 14:24:51 +0800

    Update Solution177.java

--- lintcode/Solution177.java
+++ lintcode/Solution177.java
@@ -1,29 +1,16 @@
-package src;
+public class Solution {
 
-import extend.TreeNode;
-
-public class Solution177 {
-    /*
-     * @param A: an integer array
-     * @return: A tree node
-     */
     public TreeNode sortedArrayToBST(int[] A) {
-        if (A == null || A.length == 0) {
-            return null;
-        }
-        
+        if (A == null || A.length == 0) return null;
         return helper(A, 0, A.length - 1);
     }
     
-    private TreeNode helper(int[] A, int left, int right) {
-        if (right < left) {
-            return null;
-        }
-        int mid = left + (right - left) / 2;
-        TreeNode root = new TreeNode(A[mid]);
-        root.left = helper(A, left, mid - 1);
-        root.right = helper(A, mid + 1, right);
-        
-        return root;
+    private TreeNode helper(int[] A, int start, int end) {
+        if (start > end) return null;
+        int mid = start + (end - start) / 2;
+        TreeNode node = new TreeNode(A[mid]);
+        node.left = helper(A, start, mid - 1);
+        node.right = helper(A, mid + 1, end);
+        return node;
     }
 }
