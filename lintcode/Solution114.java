8323bd4f74c32b633fcf6e272d682266e8a1abc8
cuzz1 <cuzz1234@163.com>
2019-01-09 12:38:04 +0800

    Update Solution114.java

--- lintcode/Solution114.java
+++ lintcode/Solution114.java
@@ -1,34 +1,26 @@
-package src;
-
-public class Solution114 {
+public class Solution {
     /**
      * @param m: positive integer (1 <= m <= 100)
      * @param n: positive integer (1 <= n <= 100)
      * @return: An integer
      */
-	public static void main(String[] args) {
-		int result = uniquePaths(3, 3);
-		System.out.println(result);
-	}
-	// 动态规划
-    public static int uniquePaths(int m, int n) {
-    	// 构建一个二维数组
-    	int[][] twoArray = new int[m][n];
-    	
-    	// 对数组的第一行和第一列赋值
-    	for(int i = 0; i < n; i++) {
-    		twoArray[0][i] = 1;
-    	}
-    	
-    	for (int j = 1; j < m; j++) {
-    		twoArray[j][0] = 1;
-    	}
-    	
-    	for (int i = 1; i < n; i++) {
-    		for (int j = 1; j < m; j++) {
-    			twoArray[j][i] = twoArray[j-1][i] + twoArray[j][i-1];
-    		}
-    	}
-    	return twoArray[m-1][n-1];
+    public int uniquePaths(int m, int n) {
+        if (m <= 0 || n <= 0) return 0;
+        int[][] matrix = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            matrix[i][0] = 1;
+        }
+        
+        for (int j = 0; j < n; j++) {
+            matrix[0][j] = 1;
+        }
+        
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                matrix[i][j] = matrix[i-1][j] + matrix[i][j-1];
+            }
+        }
+        
+        return matrix[m-1][n-1];
     }
 }
