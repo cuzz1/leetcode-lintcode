311049a93324130c8b98d0e46939111c38b7dabe
cuzz1 <cuzz1234@163.com>
2019-01-09 12:56:30 +0800

    Update Solution115.java

--- lintcode/Solution115.java
+++ lintcode/Solution115.java
@@ -1,54 +1,37 @@
-package src;
-
-public class Solution115 {
+public class Solution {
     /**
      * @param obstacleGrid: A list of lists of integers
      * @return: An integer
      */
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
-    	if (obstacleGrid == null && obstacleGrid.length == 0 
-    			&& obstacleGrid[0].length == 0) {
-    		return 0;
-    	}
-
-    	
-    	// 列数
-    	int m = obstacleGrid.length;
-    	// 行数
-    	int n = obstacleGrid[0].length;
-    	
-    	int[][] twoArray = new int[m][n];
-    	
-    	// 对行和列赋值
-    	twoArray[0][0] = 1;
-    	
-    	// 列赋值 
-    	for (int i = 0; i < m; i++) {
-    		if (obstacleGrid[i][0] != 1) {
-    			twoArray[i][0] = 1;
-    		} else {
-    			break;
-    		}
-    	}
-    	
-    	// 行赋值
-    	for (int j = 0; j < n; j++) {
-    		if (obstacleGrid[0][j] != 1) {
-    			twoArray[0][j] = 1;
-    		} else {
-    			break;
-    		}
-    	}
-    	
-    	for (int i = 1; i < m; i++) {
-    		for (int j = 1; j < n; j++) {
-    			if (obstacleGrid[i][j] != 1) {
-    				twoArray[i][j] = twoArray[i-1][j] + twoArray[i][j-1];
-    			} else {
-    				twoArray[i][j] = 0;
-    			}
-    		}
-    	}
-    	return twoArray[m-1][n-1];
+        if (obstacleGrid == null) return 0;
+        if (obstacleGrid.length == 0 || obstacleGrid[0].length == 0) return 0;
+        int m = obstacleGrid.length;
+        int n = obstacleGrid[0].length;
+        int[][] matrix = new int[m][n];
+        
+        for (int i = 0; i < m; i++) {
+            if (obstacleGrid[i][0] == 0) {
+                matrix[i][0] = 1;
+            } else {
+                break;
+            }
+        }
+        for (int j = 0; j < n; j++) {
+            if (obstacleGrid[0][j] == 0) {
+                matrix[0][j] = 1;
+            } else {
+                break;
+            }
+        }
+        
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (obstacleGrid[i][j] == 0) {
+                    matrix[i][j] = matrix[i-1][j] + matrix[i][j-1];
+                }
+            }
+        }
+        return matrix[m-1][n-1];
     }
 }
