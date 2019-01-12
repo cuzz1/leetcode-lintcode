ce90b68fe805465f35cffa3bf74b4b9b278da6bf
cuzz1 <cuzz1234@163.com>
2019-01-12 23:40:14 +0800

    Update Solution002.java

--- lintcode/Solution002.java
+++ lintcode/Solution002.java
@@ -1,16 +1,13 @@
-package src;
-
-public class Solution002 {
+public class Solution {
     /*
      * @param n: An integer
      * @return: An integer, denote the number of trailing zeros in n!
      */
     public long trailingZeros(long n) {
-        // 只要统计5的个数就行了
         long count = 0;
-        while (n > 0) {
+        while (n != 0) {
             count += n / 5;
-            n /= 5;
+            n = n / 5;
         }
         return count;
     }
