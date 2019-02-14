f577e503265ebe6eabfe13c9b927d483846c9ce7
cuzz1 <cuzz1234@163.com>
2019-02-14 11:49:45 +0800

    Update Solution365.java

--- lintcode/Solution365.java
+++ lintcode/Solution365.java
@@ -4,16 +4,14 @@ public class Solution {
      * @return: An integer
      */
     public int countOnes(int num) {
-        
         int count = 0;
-        int compare = 1;
-        
-        while (compare != 0) {
-            if ((num & compare) != 0) {
+        int p = 1;
+        while (p != 0) {
+            if ((num & p) != 0) {
                 count++;
-            }
-            compare = compare << 1;
+            } 
+            p = p << 1;
         }
         return count;
     }
-}
+};
