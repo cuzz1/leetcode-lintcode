35c58bd54c5346677166ff1f36a7852836976694
cuzz1 <cuzz1234@163.com>
2019-02-15 13:19:23 +0800

    Update Solution159.java

--- lintcode/Solution159.java
+++ lintcode/Solution159.java
@@ -4,18 +4,22 @@ public class Solution {
      * @return: the minimum number in the array
      */
     public int findMin(int[] nums) {
-        int left = 0;
-        int right = nums.length - 1;
+        if (nums == null || nums.length == 0) return 0;
+        if (nums.length == 1) return nums[0];
         
-        while (left + 1 < right) {
-            int mid = left + (right - left) / 2;
-            if (nums[mid] > nums[right]) {
-                left = mid;
+        int start = 0;
+        int end = nums.length - 1;
+        
+        while (start + 1 < end) {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] > nums[end]) {
+                start = mid;
             } else {
-                right = mid;
+                end = mid;
             }
+       
         }
         
-        return nums[left] > nums[right] ? nums[right] : nums[left];
+        return nums[start] < nums[end] ? nums[start] : nums[end];
     }
 }
