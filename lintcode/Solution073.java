c5f3ca0f2ce823e6afcf3fcb19add2c7bed27c2d
cuzz1 <cuzz1234@163.com>
2019-02-17 10:32:28 +0800

    Update Solution073.java

--- lintcode/Solution073.java
+++ lintcode/Solution073.java
@@ -1,15 +1,3 @@
-/**
- * Definition of TreeNode:
- * public class TreeNode {
- *     public int val;
- *     public TreeNode left, right;
- *     public TreeNode(int val) {
- *         this.val = val;
- *         this.left = this.right = null;
- *     }
- * }
- */
-
 public class Solution {
     /**
      * @param inorder: A list of integers that inorder traversal of a tree
@@ -17,16 +5,17 @@ public class Solution {
      * @return: Root of a tree
      */
     public TreeNode buildTree(int[] preorder, int[] inorder) {
-        return buildTree(preorder, 0, preorder.length - 1, inorder, 0, inorder.length - 1);
+        TreeNode node = buildTree(preorder, 0, preorder.length - 1, inorder, 0 , inorder.length - 1);
+        return node;
     }
     
-    private TreeNode buildTree(int[] preorder, int start1, int end1, int[] inorder, int start2, int end2) {
-        if (start1 > end1 || start2 > end2) return null;
-        TreeNode root = new TreeNode(preorder[start1]);
-        for (int i = start2; i <= end2; i++) {
-            if (inorder[i] == preorder[start1]) {
-                root.left = buildTree(preorder, start1 + 1, start1 + i - start2, inorder, start2, i - 1);
-                root.right = buildTree(preorder, start1 + i - start2 + 1, end1, inorder, i + 1, end2);
+    private TreeNode buildTree(int[] preorder, int preStart, int  preEnd, int[] inorder, int inStart, int inEnd) {
+        if (preStart > preEnd || inStart > inEnd) return null;
+        TreeNode root = new TreeNode(preorder[preStart]);
+        for (int i = inStart; i <= inEnd; i++) {
+            if (inorder[i] == preorder[preStart]) {
+                root.left = buildTree(preorder, preStart + 1, preStart + (i - inStart), inorder, inStart, i - 1);
+                root.right = buildTree(preorder, preStart + (i - inStart) + 1, preEnd, inorder, i + 1, inEnd);
             }
         }
         return root;
