231c9c86636fe3a6fe0ee2134008fce50ce6681e
cuzz1 <cuzz1234@163.com>
2019-02-23 10:49:50 +0800

    Update Solution165.java

--- lintcode/Solution165.java
+++ lintcode/Solution165.java
@@ -1,15 +1,3 @@
-/**
- * Definition for ListNode
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode(int x) {
- *         val = x;
- *         next = null;
- *     }
- * }
- */
-
 public class Solution {
     /**
      * @param l1: ListNode l1 is the head of the linked list
@@ -19,6 +7,7 @@ public class Solution {
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
         ListNode dummy = new ListNode(0);
         ListNode cur = dummy;
+        
         while (l1 != null && l2 != null) {
             if (l1.val < l2.val) {
                 cur.next = l1;
@@ -29,27 +18,15 @@ public class Solution {
             }
             cur = cur.next;
         }
+        
         if (l1 != null) {
             cur.next = l1;
         }
+        
         if (l2 != null) {
             cur.next = l2;
         }
         return dummy.next;
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
