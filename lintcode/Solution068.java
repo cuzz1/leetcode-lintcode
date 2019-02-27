ce685442af35de6949313c2e19f9126594be31f1
cuzz1 <cuzz1234@163.com>
2019-02-27 09:41:22 +0800

    Update Solution068.java

--- lintcode/Solution068.java
+++ lintcode/Solution068.java
@@ -1,31 +1,18 @@
-/**
- * Definition of TreeNode:
- * public class TreeNode {
- *     public int val;
- *     public TreeNode left, right;
- *     public TreeNode(int val) {
- *         this.val = val;
- *         this.left = this.right = null;
- *     }
- * }
- */
-
 public class Solution {
     /**
      * @param root: A Tree
      * @return: Postorder in ArrayList which contains node values.
      */
     public List<Integer> postorderTraversal(TreeNode root) {
-        List<Integer> list = new ArrayList<>();
-        postorder(list, root);
-        return list;
+        List<Integer> res = new ArrayList<>();
+        helper(root, res);
+        return res;
     }
     
-    private void postorder(List<Integer> list, TreeNode node) {
-        if (node == null) return;
-        if (node.left != null) postorder(list, node.left);
-        if (node.right != null) postorder(list, node.right);
-        list.add(node.val);
+    private void helper(TreeNode root, List<Integer> res) {
+        if (root == null) return;
+        helper(root.left, res);
+        helper(root.right, res);
+        res.add(root.val);
     }
-    
 }
