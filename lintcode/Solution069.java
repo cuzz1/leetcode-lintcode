f8d5768ea28b6001edc684d2e695cb1771184fd0
cuzz1 <cuzz1234@163.com>
2019-02-27 09:33:48 +0800

    Update Solution069.java

--- lintcode/Solution069.java
+++ lintcode/Solution069.java
@@ -5,17 +5,18 @@ public class Solution {
      */
     public List<List<Integer>> levelOrder(TreeNode root) {
         List<List<Integer>> res = new ArrayList<>();
-        if (root == null) return res;
         Queue<TreeNode> queue = new LinkedList<>();
+        if (root == null) return res;
         queue.offer(root);
         while (!queue.isEmpty()) {
             int size = queue.size();
             List<Integer> list = new ArrayList<>();
-            while (size-- > 0) {
+            while (size > 0) {
                 TreeNode node = queue.poll();
                 list.add(node.val);
-                if (node.left != null) queue.offer(node.left);
+                if (node.left != null) queue.offer(node.left); 
                 if (node.right != null) queue.offer(node.right);
+                size--;
             }
             res.add(list);
         }
