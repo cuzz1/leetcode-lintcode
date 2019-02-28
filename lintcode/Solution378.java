5c0e71f6b7764d0462d73b5908c062fb4b041522
cuzz1 <cuzz1234@163.com>
2019-02-28 11:05:38 +0800

    Update Solution378.java

--- lintcode/Solution378.java
+++ lintcode/Solution378.java
@@ -52,3 +52,36 @@ public class Solution {
         inOrder(node.right);
     }
 }
+
+public class Solution {
+    /**
+     * @param root: The root of tree
+     * @return: the head of doubly list node
+     */
+    public DoublyListNode bstToDoublyList(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        inorder(root, list);
+        
+        return build(list);
+    }
+    
+    private void inorder(TreeNode root, List<Integer> list) {
+        if (root == null) return;
+        inorder(root.left, list);
+        list.add(root.val);
+        inorder(root.right, list);
+    }
+    
+    private DoublyListNode build(List<Integer> list) {
+        if (list == null || list.size() == 0) return null;
+        DoublyListNode node = new DoublyListNode(list.get(0));
+        DoublyListNode cur = node;
+        for (int i = 1; i < list.size(); i++) {
+            DoublyListNode temp = new DoublyListNode(list.get(i)); 
+            cur.next = temp;
+            temp.prev = cur;
+            cur = cur.next;
+        }
+        return node;
+    }
+}
