975ca4422d63dabf43e6932e76c420e34ae88e4c
cuzz1 <cuzz1234@163.com>
2019-03-01 08:57:21 +0800

    Update Solution012.java

--- lintcode/Solution012.java
+++ lintcode/Solution012.java
@@ -1,21 +1,23 @@
 public class MinStack {
     
-    private Stack<Integer> stack1; 
-    private Stack<Integer> stack2; 
+    Stack<Integer> stack1 = new Stack<>();
+    Stack<Integer> stack2 = new Stack<>();
     
     public MinStack() {
-        this.stack1 = new Stack<>();
-        this.stack2 = new Stack<>();
+        // do intialization if necessary
     }
 
+    /*
+     * @param number: An integer
+     * @return: nothing
+     */
     public void push(int number) {
         stack1.push(number);
-        
         if (stack2.isEmpty()) {
             stack2.push(number);
             return;
-        }
-             
+        } 
+        
         if (number < stack2.peek()) {
             stack2.push(number);
         } else {
@@ -23,11 +25,17 @@ public class MinStack {
         }
     }
 
+    /*
+     * @return: An integer
+     */
     public int pop() {
         stack2.pop();
         return stack1.pop();
     }
 
+    /*
+     * @return: An integer
+     */
     public int min() {
         return stack2.peek();
     }
