8043ee8676f2c1cb670fb734ff253e7703f36f9d
cuzz1 <cuzz1234@163.com>
2019-03-06 09:38:57 +0800

    Update Solution379.java

--- lintcode/Solution379.java
+++ lintcode/Solution379.java
@@ -4,23 +4,21 @@ public class Solution {
      * @return: A string
      */
     public String minNumber(int[] nums) {
+        if (nums == null || nums.length == 0) return "";
         
-        if (nums == null || nums.length == 0) return "0";
-        
-        String[] strs = new String[nums.length];
-        
+        String[] strs = new String[nums.length]; 
         for (int i = 0; i < nums.length; i++) {
             strs[i] = nums[i] + "";
         }
         
-        Arrays.sort(strs,(a, b) -> (a + b).compareTo(b + a));
+        Arrays.sort(strs, (a, b) -> (a + b).compareTo(b + a));
         
         String res = "";
-        for (String str : strs) {
-            if (res.length() == 0 && str.equals("0")) {
-                continue;
-            }
-            res += str;
+        for (int i = 0; i < strs.length; i++) {
+           if (res.length() == 0 && strs[i].equals("0")) {
+               continue;
+           }
+           res += strs[i]; 
         }
         return res.length() == 0 ? "0" : res;
     }
