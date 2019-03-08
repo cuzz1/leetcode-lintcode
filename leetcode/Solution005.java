d0a50b7e3097a5020640ced017f28a08f5bf0621
cuzz1 <cuzz1234@163.com>
2019-03-08 19:39:23 +0800

    Update Solution005.java

--- leetcode/Solution005.java
+++ leetcode/Solution005.java
@@ -1,23 +1,20 @@
 class Solution {
     public String longestPalindrome(String s) {
-        if (s == null || s.length() == 0) {
-            return s;
-        }
-        
-        String res = "";
-        int max = 0;
+        if (s == null || s.length() == 0) return s;
         boolean[][] dp = new boolean[s.length()][s.length()];
-        
-        for (int j = 0; j < dp.length; j++) {
-            for (int i = 0; i <= j; i ++) {
-                dp[i][j] =  s.charAt(i) == s.charAt(j) && ((j - i <= 2) || dp[i + 1][j - 1]); 
-                if (dp[i][j] && (j - i + 1) > max){
+        int max = 0;
+        String res = "";
+        // a b b a
+        // i     j
+        for (int j = 0; j < s.length(); j++) {
+            for (int i = 0; i <= j; i++) {
+                dp[i][j] =  s.charAt(i) == s.charAt(j) && (j - i <= 2 || dp[i + 1][j - 1]);
+                if (dp[i][j] && j - i + 1 > max) {
                     max = j - i + 1;
-                    res = s.substring(i, j + 1);
+                    res = s.substring(i, j + 1); 
                 }
             }
         }
-        
         return res;
     }
 }
