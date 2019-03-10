9d6aa0bad701f9985e842d17cc54ab4046d3cf34
cuzz1 <cuzz1234@163.com>
2019-03-10 10:16:19 +0800

    Update Solution097.java

--- lintcode/Solution097.java
+++ lintcode/Solution097.java
@@ -1,15 +1,3 @@
-/**
- * Definition of TreeNode:
- * public class TreeNode {
- *     public int val;
- *     public TreeNode left, right;
- *     public TreeNode(int val) {
- *         this.val = val;
- *         this.left = this.right = null;
- *     }
- * }
- */
-// 分治思想
 public class Solution {
     /**
      * @param root: The root of binary tree.
