47481ec718c329c03dce0fa1f7c8fbb46427aeb8
cuzz1 <cuzz1234@163.com>
2019-03-10 10:20:16 +0800

    Update Solution082.java

--- lintcode/Solution082.java
+++ lintcode/Solution082.java
@@ -1,11 +1,14 @@
 public class Solution {
-    // a ^ b ^ b = a
+    /**
+     * @param A: An integer array
+     * @return: An integer
+     */
     public int singleNumber(int[] A) {
-        if (A == null || A.length == 0) return 0;
-        int n = A[0];
+        if (A == null || A.length == 0) return Integer.MAX_VALUE;
+        int res = A[0];
         for (int i = 1; i < A.length; i++) {
-            n ^= A[i];
+            res ^= A[i];
         }
-        return n;
+        return res;
     }
 }
