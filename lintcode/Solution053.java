99aeec063c78f300c960b24a46b2a3a88ec441ca
cuzz1 <cuzz1234@163.com>
2019-03-11 11:53:28 +0800

    Update Solution053.java

--- lintcode/Solution053.java
+++ lintcode/Solution053.java
@@ -4,21 +4,25 @@ public class Solution {
      * @return: A string
      */
     public String reverseWords(String s) {
+        
+        String[] strs = s.split("\\s+");
         String res = "";
-        String str = reverse(s.trim()); 
-        String[] strs = str.split("\\s+");
-        for (int i = 0; i < strs.length; i++) {
-            res += reverse(strs[i]) + " ";
+        for (String str : strs) {
+            res += reverse(str) + " ";
         }
-        return res.substring(0, res.length() - 1);
+        return reverse(res.trim());
     }
     
-    private String reverse(String s) {
-        char[] chars = s.toCharArray();
-        for (int i = 0; i < chars.length / 2; i++) {
+    private String reverse(String str) {
+        char[] chars = str.toCharArray();
+        int i = 0;
+        int j = chars.length - 1;
+        while (i < j) {
             char temp = chars[i];
-            chars[i] = chars[chars.length - 1- i];
-            chars[chars.length - 1 - i] = temp;
+            chars[i] = chars[j];
+            chars[j] = temp;
+            i++;
+            j--;
         }
         return new String(chars);
     }
