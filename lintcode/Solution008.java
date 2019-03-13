f9f41ea41c7f9e815609765cc2f8f3183f47473d
cuzz1 <cuzz1234@163.com>
2019-03-13 09:16:13 +0800

    Update Solution008.java

--- lintcode/Solution008.java
+++ lintcode/Solution008.java
@@ -6,21 +6,21 @@ public class Solution {
      */
     public void rotateString(char[] str, int offset) {
         if (str == null || str.length == 0) return;
-        offset = str.length - offset % str.length;
+        
+        offset %= str.length;
+        
+        reverse(str, 0, str.length - 1);
         reverse(str, 0, offset - 1);
         reverse(str, offset, str.length - 1);
-        reverse(str, 0, str.length - 1);
     }
     
     private void reverse(char[] str, int start, int end) {
         while (start < end) {
-            swap(str, start++, end--);
+            char temp = str[start];
+            str[start] = str[end];
+            str[end] = temp;
+            start++;
+            end--;
         }
-    }
-    
-    private void swap(char[] str, int i, int j) {
-        char c = str[i];
-        str[i] = str[j];
-        str[j] = c;
     }
 }
