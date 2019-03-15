21ce5bca2bfbbe3eff9c2932c27c2edd2d2a6989
cuzz1 <cuzz1234@163.com>
2019-03-15 22:52:15 +0800

    Update Solution061.java

--- lintcode/Solution061.java
+++ lintcode/Solution061.java
@@ -5,37 +5,50 @@ public class Solution {
      * @return: a list of length 2, [index1, index2]
      */
     public int[] searchRange(int[] A, int target) {
-        if (A == null || A.length == 0) return new int[]{-1, -1};
-        return new int[]{findMin(A, target), findMax(A, target)};
-    }
-    
-    private int findMin(int[] A, int target) {
+        int[] res = {-1, -1};
+        if (A == null || A.length == 0) return res;
         int start = 0;
         int end = A.length - 1;
         while (start + 1 < end) {
             int mid = start + (end - start) / 2;
-            if (A[mid] >= target) {
+            if (A[mid] < target) {
+                start = mid;
+            } else if (A[mid] > target) {
+                end = mid;
+            } else {
+                end = mid;
+            }
+        }
+        
+        if (A[start] == target) {
+            res[0] = start;
+        } else if (A[end] == target){
+            res[0] = end;
+        } else {
+            res[0] = -1;
+        }
+        
+        start = 0;
+        end = A.length - 1;
+        while (start + 1 < end) {
+            int mid = start + (end - start) / 2;
+            if (A[mid] < target) {
+                start = mid;
+            } else if (A[mid] > target) {
                 end = mid;
             } else {
                 start = mid;
             }
         }
-        if (A[start] == target) return start;
-        return A[end] == target ? end : -1;
-    }
-    
-    private int findMax(int[] A, int target) {
-        int start = 0;
-        int end = A.length - 1;
-        while (start + 1 < end) {
-            int mid = start + (end - start) / 2;
-            if (A[mid] > target) {
-                end = mid;
-            } else {
-                start = mid;
-            }
+
+        if (A[end] == target) {
+            res[1] = end;
+        } else if (A[start] == target){
+            res[1] = start;
+        } else {
+            res[1] = -1;
         }
-        if (A[end] == target) return end;
-        return A[start] == target ? start : -1;
+        return res;
+        
     }
 }
