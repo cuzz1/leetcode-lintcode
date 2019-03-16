734e64a58b6d77a0a7ad9d479ceb801c3954dfc2
cuzz1 <cuzz1234@163.com>
2019-03-16 11:07:36 +0800

    Update Solution088.java

--- lintcode/Solution088.java
+++ lintcode/Solution088.java
@@ -1,10 +1,12 @@
 public class Solution {
     // 分治的思想
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode A, TreeNode B) {
-        if (root == null || root == A || root == B) return root;
+        if (root == null || A == null || B == null) return root;
         TreeNode left = lowestCommonAncestor(root.left, A, B);
         TreeNode right = lowestCommonAncestor(root.right, A, B);
-        if (left != null && right != null) return root;
+        if (left != null && right != null) {
+            return root;
+        }
         return left == null ? right : left;
     }
 }
