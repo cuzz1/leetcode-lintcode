018d410bbd056670f68772e550a021d2049d79c1
cuzz1 <cuzz1234@163.com>
2019-03-16 11:15:23 +0800

    Update Solution088.java

--- lintcode/Solution088.java
+++ lintcode/Solution088.java
@@ -1,12 +1,16 @@
 public class Solution {
-    // 分治的思想
+    /*
+     * @param root: The root of the binary search tree.
+     * @param A: A TreeNode in a Binary.
+     * @param B: A TreeNode in a Binary.
+     * @return: Return the least common ancestor(LCA) of the two nodes.
+     */
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode A, TreeNode B) {
-        if (root == null || A == null || B == null) return root;
+        if (root == null || A == root || B == root) return root;
         TreeNode left = lowestCommonAncestor(root.left, A, B);
         TreeNode right = lowestCommonAncestor(root.right, A, B);
-        if (left != null && right != null) {
-            return root;
-        }
-        return left == null ? right : left;
+        // 左边和右边各找到一个
+        if (left !=null && right != null) return root;
+        return left != null ? left : right;
     }
 }
