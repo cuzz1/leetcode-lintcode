8c09a72f3ccb1c1ca303e8fb3c212d58fc864981
cuzz1 <cuzz1234@163.com>
2019-03-17 14:30:01 +0800

    Update Solution112.java

--- lintcode/Solution112.java
+++ lintcode/Solution112.java
@@ -3,14 +3,15 @@ public class Solution {
      * @param head: head is the head of the linked list
      * @return: head of linked list
      */
-    public ListNode deleteDuplicates(ListNode head) {
-        if (head == null) return head;
-        ListNode node = head;
-        while (node != null) {
-            while (node.next != null && node.val == node.next.val) {
-                node.next = node.next.next;
+    public ListNode deleteDuplicates(ListNode head) {   
+        if (head == null) return head; 
+        
+        ListNode cur = head;
+        while (cur != null) {
+            while (cur != null && cur.next != null && cur.val == cur.next.val) {
+                cur.next = cur.next.next;
             }
-            node = node.next;
+            cur = cur.next;
         }
         return head;
     }
