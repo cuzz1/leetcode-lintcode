e18631b2ca1646a241981d24cce97223bdad0257
cuzz1 <cuzz1234@163.com>
2019-03-17 14:32:11 +0800

    Update Solution112.java

--- lintcode/Solution112.java
+++ lintcode/Solution112.java
@@ -8,7 +8,7 @@ public class Solution {
         
         ListNode cur = head;
         while (cur != null) {
-            while (cur != null && cur.next != null && cur.val == cur.next.val) {
+            while (cur.next != null && cur.val == cur.next.val) {
                 cur.next = cur.next.next;
             }
             cur = cur.next;
