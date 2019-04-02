846201eb45be469a5d5095e8c5bdc271477e24ae
cuzz1 <cuzz1234@163.com>
2019-04-02 15:38:31 +0800

    Create Solution094.java

---
+++ leetcode/Solution094.java
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        if (root == null) return res;
+        helper(root, res);
+        return res;
+    }
+    
+    private void helper(TreeNode root, List<Integer> res) {
+        if (root == null) return;
+        helper(root.left, res);
+        res.add(root.val);
+        helper(root.right, res);
+    }
+}
