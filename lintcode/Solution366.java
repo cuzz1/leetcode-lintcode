8bbef72531376e8bb7a268ef9358f69796473134
cuzz1 <cuzz1234@163.com>
2019-07-21 23:58:38 +0800

    366. 斐波纳契数列

--- lintcode/Solution366.java
+++ lintcode/Solution366.java
@@ -3,14 +3,26 @@ public class Solution {
      * @param n: an integer
      * @return: an ineger f(n)
      */
+    // public int fibonacci(int n) {
+    //     if (n == 1) return 0; 
+    //     if (n == 2) return 1;
+    //     return fibonacci(n-1) + fibonacci(n-2);
+    // }
+    
+    /**
+     * @param n: an integer
+     * @return: an ineger f(n)
+     */
     public int fibonacci(int n) {
         
+        int i = 1;
         int a = 0;
         int b = 1;
-        for (int i = 1; i < n; i++) {
-            int temp = a + b;
-            a = b;
-            b = temp;
+        while (i < n) {
+            int temp = a;
+            a =  b;
+            b = temp + b;
+            i++;
         }
         return a;
     }
