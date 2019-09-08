f4dcd45a653fd29735c5bfc1b5164bbf4c9d74ae
cuzz1 <cuzz1234@163.com>
2019-09-08 10:57:09 +0800

    1. 两数之和

--- leetcode-cn/Solution01.java
+++ leetcode-cn/Solution01.java
@@ -1,1 +1,19 @@
-xxxxxsdfsdfsdf
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        
+        if (nums.length < 2) {
+            return new int[] {0, 0};
+        }
+        
+        Map<Integer, Integer> map = new HashMap<>();
+        
+        for (int i = 0; i < nums.length; i++) {
+            int val = target - nums[i];
+            if (map.containsKey(val)) {
+                return new int[] {map.get(val), i};
+            } 
+            map.put(nums[i], i);
+        }
+       return new int[]{0, 0}; 
+    }
+}
