bb905f64f34aae1e714e70eb2d884ca5e65fd361
cuzz1 <cuzz1234@163.com>
2020-08-10 10:41:11 +0800

    Update and rename Solution175.sql to Solution0175.sql

---
+++ leetcode-cn/Solution0175.sql
@@ -0,0 +1,1 @@
+select p.FirstName, p.lastName, a.City, a.state from Person as p left join Address as a on p.PersonId = a.PersonId
--- leetcode-cn/Solution175.sql
+++
@@ -1,1 +0,0 @@
-select p.FirstName, p.lastName, a.City, a.state from Person as p left join Address as a on p.PersonId = a.PersonI
