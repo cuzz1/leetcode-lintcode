73ce4031ef0f15ea73a0ac98bb8812ddb3051152
cuzz1 <cuzz1234@163.com>
2020-12-31 16:31:55 +0800

    Update Solution0001.java

--- leetcode-cn/Solution0001.java
+++ leetcode-cn/Solution0001.java
@@ -1,19 +1,20 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        
+        int[] res = new int[2];
+
         if (nums.length < 2) {
-            return new int[] {0, 0};
+            return res;
         }
-        
-        Map<Integer, Integer> map = new HashMap<>();
-        
+
+        // cache
+        Map<Integer, Integer> cache = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
-            int val = target - nums[i];
-            if (map.containsKey(val)) {
-                return new int[] {map.get(val), i};
-            } 
-            map.put(nums[i], i);
+            int key = target - nums[i];
+            if (cache.containsKey(key)) {
+                return new int[]{cache.get(key), i};
+            }
+            cache.put(nums[i], i);
         }
-       return new int[]{0, 0}; 
+        return res;
     }
 }
