4535b4758294bb470cf61753ec02fdcbafd54565
cuzz1 <cuzz1234@163.com>
2020-12-31 17:12:59 +0800

    Update solution0001.go

--- leetcode-cn-go/solution0001.go
+++ leetcode-cn-go/solution0001.go
@@ -1,1 +1,18 @@
-
+func twoSum(nums []int, target int) []int {
+    res := make([]int, 2)
+    size := len(nums)
+    if size < 2 {
+        return res
+    }
+    cache := make(map[int]int)
+    for i := 0; i < size; i++ {
+        key := target - nums[i]
+        if v, ok := cache[key]; ok {
+            res[0] = v; 
+            res[1] = i;
+            return res
+        }
+        cache[nums[i]] = i
+    }
+    return res
+}
