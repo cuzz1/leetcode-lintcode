7b0184a20e2233bb56a9c79126ebccfa7da4d962
cuzz1 <cuzz1234@163.com>
2023-11-20 20:42:29 +0800

    Update Solution0002.java

--- leetcode-cn/Solution0002.java
+++ leetcode-cn/Solution0002.java
@@ -1,38 +1,25 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode pre = new ListNode(0);
-
-        ListNode cur = pre;
-        int carry = 0;
-        while(l1 != null || l2 != null) {
-            int a1 = l1 == null ? 0 : l1.val;
-            int a2 = l2 == null ? 0 : l2.val;
-
-            int sum = a1 + a2 + carry;
-
-            int val = sum % 10;
-            carry = sum / 10;
-            ListNode node = new ListNode(val);
-            cur.next = node;
+        ListNode dummy = new ListNode(0);
+        ListNode cur = dummy;
+        int plus = 0;
+        while (l1 != null || l2 != null) {
+            int l1Val = (l1 != null) ? l1.val : 0;
+            int l2Val = (l2 != null) ? l2.val : 0;
+            int sum = l1Val + l2Val + plus;
+            cur.next = new ListNode(sum % 10);
+            plus = sum / 10;
             cur = cur.next;
-            if (l1 != null) l1 = l1.next;
-            if (l2 != null) l2 = l2.next;
+            if (l1 != null) {
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                l2 = l2.next;
+            }
         }
-        if (carry == 1) {
+        if (plus == 1) {
             cur.next = new ListNode(1);
         }
-
-        return pre.next;
-
+        return dummy.next;
     }
 }
